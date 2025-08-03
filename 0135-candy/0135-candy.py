b6e3d3b7404760ada8a078ad9006a80671e541f6
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-03 16:07:34 +0100

    Time: 15 ms (72.35%), Space: 19.8 MB (69.63%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -7,12 +7,9 @@ class Solution:
         for i in range(n - 1):
             if ratings[i + 1] > ratings[i]:
                 candies[i+1] = candies[i] + 1
-        print(candies)
         
         # loop backwards
         for i in range(n-1,0,-1):
             if ratings[i-1] > ratings[i]:
-                maximum  = max(candies[i-1], candies[i] + 1)
-                candies[i-1] = maximum
-        print(candies)
+                candies[i-1] = max(candies[i-1], candies[i] + 1)
         return sum(candies)
