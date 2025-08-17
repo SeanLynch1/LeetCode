ad9ab9502dfd105573ddf01806cb345ba65fb0d8
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-17 22:19:55 +0100

    Time: 9 ms (44.89%), Space: 18.2 MB (56.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alphabet = {
-                    'a':'a', 'b':'b', 'c':'c', 'd':'d', 'e':'e', 'f':'f', 'g':'g',
-                    'h':'h', 'i':'i', 'j':'j', 'k':'k', 'l':'l', 'm':'m', 'n':'n',
-                    'o':'o', 'p':'p', 'q':'q', 'r':'r', 's':'s', 't':'t', 'u':'u',
-                    'v':'v', 'w':'w', 'x':'x', 'y':'y', 'z':'z'
-                    }
-
         left = 0
         right = len(s) -1
         output = True
@@ -16,20 +9,13 @@ class Solution:
             right_letter = s[right]
 
             if not left_letter.isalnum():
-                print("left_letter = ", left_letter)
                 left += 1
             elif not right_letter.isalnum():
-                print("right_letter = ", right_letter)
                 right -= 1
             elif left_letter.lower() == right_letter.lower():
-                print(left_letter.lower(), " == ", right_letter.lower())
                 left += 1
                 right -= 1
             else:
-                print("false")
-                print("FALSE")
                 return False
 
-            print("\n")
-        print("returning output")
         return output
