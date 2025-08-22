29d52954c57ec4671cc4f6b3f49e34f66834b727
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-22 23:17:01 +0100

    Time: 453 ms (10.49%), Space: 17.8 MB (48.91%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -5,11 +5,6 @@ class Solution:
         closest_sum = sum(nums[:3])
         closest_distance = abs(target - closest_sum)
 
-        print("nums = ", nums)
-        print("closest_sum = ", closest_sum)
-        print("closest_distance = ", closest_distance)
-        print("\n")
-
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
@@ -21,18 +16,13 @@ class Solution:
                 
                 remaining = target - nums[i]
 
-                print("j = ", j, "k = ", k)
 
                 curr_sum = (nums[i] + nums[j] + nums[k])
 
-                print(f"current sum of {nums[i]}, {nums[j]}, {nums[k]} = ", curr_sum)
 
                 if abs(target - curr_sum) < closest_distance:
                     closest_distance = abs(target - curr_sum)
                     closest_sum = curr_sum
-
-                    print("closest_sum = ", closest_sum)
-                    print("closest distance = ", closest_distance, "\n")  
 
                 if nums[j] + nums[k] < remaining: 
                     j += 1
