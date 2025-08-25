b9788de605d094da3285f15a63ab567ac2fc9f79
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-25 22:30:41 +0100

    Time: 79 ms (50.85%), Space: 23.2 MB (6.82%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,47 @@
-from collections import Counter
+from collections import defaultdict, deque
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        
         if not s or not t:
             return ""
-        
-        need = Counter(t)
-        missing = len(t)
-        left = start = end = 0
-        
-        for right, char in enumerate(s, 1):
-            if need[char] > 0:
-                missing -= 1
-            need[char] -= 1
+
+
+        found_letters = defaultdict(lambda: [0, 0])
+
+        count = 0
+        sub_string = deque()
+
+        start = 0
+        end = 0
+
+        for letter in t:
+            a, b = found_letters[letter]
+            found_letters[letter] = [0, b + 1]
+
+        for right, char in enumerate(s, 0):
             
-            # When all chars matched
-            if missing == 0:
-                # shrink left pointer
-                while left < right and need[s[left]] < 0:
-                    need[s[left]] += 1
-                    left += 1
-                if end == 0 or right - left < end - start:
-                    start, end = left, right
-                # Move left for next round
-                need[s[left]] += 1
-                missing += 1
-                left += 1
-        
+            if char in found_letters:
+                if found_letters[char][0] < found_letters[char][1]:
+                    count += 1
+
+                found_letters[char][0] += 1
+                sub_string.append([char, right])
+
+                while count == len(t):
+                    
+                    if (end - start) > (right - sub_string[0][1]) or (end - start) == 0:
+                        start = sub_string[0][1]
+                        end = right + 1
+
+                    first_sub_letter = sub_string[0][0]
+
+                    found_letters[first_sub_letter][0] -= 1
+
+                    if found_letters[first_sub_letter][0] < found_letters[first_sub_letter][1]:
+                        count -= 1
+                    
+                    # sets left to next value
+                    sub_string.popleft()
+                
         return s[start:end]
