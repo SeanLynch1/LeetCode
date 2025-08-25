4d787fcbb14fffe50d541b3d2b1556d1c5b0a619
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-25 22:14:29 +0100

    Time: 152 ms (23.9%), Space: 22.7 MB (6.82%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,11 +3,17 @@ from collections import defaultdict, deque
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
+        if not s or not t:
+            return ""
+
+
         found_letters = defaultdict(lambda: [0, 0])
 
-        min_substring = ""
         count = 0
         sub_string = deque()
+
+        start = 0
+        end = 0
 
         for letter in t:
             a, b = found_letters[letter]
@@ -23,9 +29,12 @@ class Solution:
                 sub_string.append((s[i], i))
 
                 while count == len(t):
-
-                    if len(min_substring) > len(s[sub_string[0][1]:i+1]) or min_substring == "":
-                        min_substring = s[sub_string[0][1]:i+1]
+                    
+                    if (end - start) > (i - sub_string[0][1]) or (end - start) == 0:
+                        start = sub_string[0][1]
+                        print(i)
+                        end = i + 1
+                        print("start = ", start)
 
                     first_sub_letter = sub_string[0][0]
 
@@ -37,4 +46,4 @@ class Solution:
                     # sets left to next value
                     sub_string.popleft()
                 
-        return min_substring
+        return s[start:end]
