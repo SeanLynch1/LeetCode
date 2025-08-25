54535f356169b9a984cb3954bd07726fb1f9feaa
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-25 22:30:36 +0100

    Time: 62 ms (81.65%), Space: 18.1 MB (93.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,47 +1,30 @@
-from collections import defaultdict, deque
+from collections import Counter
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
         if not s or not t:
             return ""
-
-
-        found_letters = defaultdict(lambda: [0, 0])
-
-        count = 0
-        sub_string = deque()
-
-        start = 0
-        end = 0
-
-        for letter in t:
-            a, b = found_letters[letter]
-            found_letters[letter] = [0, b + 1]
-
-        for i in range(len(s)):
+        
+        need = Counter(t)
+        missing = len(t)
+        left = start = end = 0
+        
+        for right, char in enumerate(s, 1):
+            if need[char] > 0:
+                missing -= 1
+            need[char] -= 1
             
-            if s[i] in found_letters:
-                if found_letters[s[i]][0] < found_letters[s[i]][1]:
-                    count += 1
-
-                found_letters[s[i]][0] += 1
-                sub_string.append((s[i], i))
-
-                while count == len(t):
-                    
-                    if (end - start) > (i - sub_string[0][1]) or (end - start) == 0:
-                        start = sub_string[0][1]
-                        end = i + 1
-
-                    first_sub_letter = sub_string[0][0]
-
-                    found_letters[first_sub_letter][0] -= 1
-
-                    if found_letters[first_sub_letter][0] < found_letters[first_sub_letter][1]:
-                        count -= 1
-                    
-                    # sets left to next value
-                    sub_string.popleft()
-                
+            # When all chars matched
+            if missing == 0:
+                # shrink left pointer
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+                if end == 0 or right - left < end - start:
+                    start, end = left, right
+                # Move left for next round
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+        
         return s[start:end]
