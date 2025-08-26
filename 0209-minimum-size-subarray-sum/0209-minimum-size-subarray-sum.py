8e22b6709be9776870eeda97e2b61d4331a5bdad
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-26 14:20:43 +0100

    Time: 8 ms (65.93%), Space: 28.3 MB (80.13%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,29 +5,17 @@ class Solution:
         start, end, left, right = 0, 0, 0, 0
         summation = 0
 
-        while right < len(nums):
-            
-
+        for right in range(len(nums)):
             summation += nums[right]
-            print("before: ")
-            print("left = ", left, ", right =", right)
-            print("summation = ", summation, "target = ", target)
+
             while summation >= target:
                 
                 if right - left < end - start or end == 0:
-                    print("hello")
                     start, end = left, right
                     output = end - start + 1
-                    print(start, end, output)
 
                 summation -= nums[left]
                 left += 1
-                print("left = ", left)
-                print("summation = ", summation)
-
-            right += 1
-
-            print("\n")
         
         return output
 
