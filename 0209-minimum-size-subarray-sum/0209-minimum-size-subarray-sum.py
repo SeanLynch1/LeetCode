e7413530cf890f5c86dff5d559fac4539635882b
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-26 14:20:29 +0100

    Time: 375 ms (5.09%), Space: 28.3 MB (80.13%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -12,19 +12,18 @@ class Solution:
             print("before: ")
             print("left = ", left, ", right =", right)
             print("summation = ", summation, "target = ", target)
-            while summation > target:
-                print("hello?")
+            while summation >= target:
+                
+                if right - left < end - start or end == 0:
+                    print("hello")
+                    start, end = left, right
+                    output = end - start + 1
+                    print(start, end, output)
+
                 summation -= nums[left]
                 left += 1
                 print("left = ", left)
                 print("summation = ", summation)
-
-                if summation >= target:
-                    if right - left < end - start or end == 0:
-                        print("hello")
-                        start, end = left, right
-                        output = end - start + 1
-                        print(start, end, output)
 
             right += 1
 
