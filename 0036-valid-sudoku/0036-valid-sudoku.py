fb94ea699f3c9fed32989c209278b744f45b71cc
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-27 13:35:36 +0100

    Time: 9 ms (13.03%), Space: 17.9 MB (44.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,45 +1,34 @@
 from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = {}
-        cols = {}
-        squares = {}
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
             
         offset = 0
         for i in range(len(board)):
-            rows[i] = defaultdict(int)
-
             square_no = 0 + offset
 
-            if square_no not in squares:
-                squares[i] = defaultdict(int)
-
             for j in range(len(board[i])):
-                if j not in cols:
-                    cols[j] = defaultdict(int)
-
                 # squares
                 if board[i][j] != ".":
                     if board[i][j] not in squares[square_no]:
-                        squares[square_no][board[i][j]] += 1
+                        squares[square_no].add(board[i][j])
                     else:
                         return False
 
                 if j < (len(board[i]) - 1) and (j + 1) % 3 == 0:
                     square_no += 1
 
-                    if square_no not in squares:
-                        squares[square_no] = defaultdict(int)
-                    
                 # rows and cols
                 if board[i][j] != ".":
                     if board[i][j] not in rows[i]:
-                        rows[i][board[i][j]] += 1
+                        rows[i].add(board[i][j])
                     else:
                         return False
 
                     if board[i][j] not in cols[j]:
-                        cols[j][board[i][j]] += 1
+                        cols[j].add(board[i][j])
                     else:
                         return False
 
