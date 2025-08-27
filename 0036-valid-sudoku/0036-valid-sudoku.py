26b54928bb8045b9e251b74f9ddcf41a7ec85082
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-27 13:41:48 +0100

    Time: 5 ms (48.35%), Space: 17.7 MB (69.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,29 +4,28 @@ class Solution:
         rows = defaultdict(set)
         cols = defaultdict(set)
         squares = defaultdict(set)
-
+        
         for i in range(len(board)):
             for j in range(len(board[i])):
                 square_no = (i // 3) * 3 + (j // 3)
 
                 # squares
-                if board[i][j] == ".":
-                    continue
-
-                if board[i][j] not in squares[square_no]:
-                    squares[square_no].add(board[i][j])
-                else:
-                    return False
+                if board[i][j] != ".":
+                    if board[i][j] not in squares[square_no]:
+                        squares[square_no].add(board[i][j])
+                    else:
+                        return False
 
                 # rows and cols
-                if board[i][j] not in rows[i]:
-                    rows[i].add(board[i][j])
-                else:
-                    return False
+                if board[i][j] != ".":
+                    if board[i][j] not in rows[i]:
+                        rows[i].add(board[i][j])
+                    else:
+                        return False
 
-                if board[i][j] not in cols[j]:
-                    cols[j].add(board[i][j])
-                else:
-                    return False
+                    if board[i][j] not in cols[j]:
+                        cols[j].add(board[i][j])
+                    else:
+                        return False
 
         return True
