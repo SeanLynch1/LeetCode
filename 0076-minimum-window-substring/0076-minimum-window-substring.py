d5bea8ce339ff35d7e19c76af528caa15517c503
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-27 11:01:58 +0100

    Time: 111 ms (39.72%), Space: 18.2 MB (77.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,44 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        
+        if not t or len(t) > len(s):
+            return ""
 
         t_tracker = Counter(t)
 
+        missing = len(t)
         left, start, end = 0, 0, 0
 
-        missing = len(t)
-
-        # offset by 1 because end is exlusive when slicing, index represents end
         for right, char in enumerate(s, 1):
-
+            
             if t_tracker[char] > 0:
                 missing -= 1
-
+            
             t_tracker[char] -= 1
 
-            # if every letter in t is in use
             if missing == 0:
-                while left < right and (t_tracker[s[left]] < 0):
+                print(f"right = {right}, left = {left}")
+                print(f"s[left] = {s[left]}")
+                print(t_tracker)
+                while t_tracker[s[left]] < 0:
                     t_tracker[s[left]] += 1
                     left += 1
-                
+
+                if t_tracker[s[left]] == 0:
+                    t_tracker[s[left]] += 1
+                    missing += 1
+
                 if right - left < end - start or end == 0:
                     start, end = left, right
-
-                # exclude the left most letter for the next iteration
-                t_tracker[s[left]] += 1
+                    print("updating word to ", s[start:end])
+                
                 left += 1
-                missing += 1
+
+                print(t_tracker, "missing = ", missing, "left = ", left)
+                print("\n")
 
         return s[start:end]
+            
+
