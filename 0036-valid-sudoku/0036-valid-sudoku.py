fc7d32a0c1957da7a09a9ee3c578905ded95f2f0
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-27 13:45:50 +0100

    Time: 7 ms (37.26%), Space: 18 MB (25.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -7,25 +7,19 @@ class Solution:
         
         for i in range(len(board)):
             for j in range(len(board[i])):
+
+                val = board[i][j]
+                # squares
+                if val == ".":
+                    continue
+
                 square_no = (i // 3) * 3 + (j // 3)
 
-                # squares
-                if board[i][j] != ".":
-                    if board[i][j] not in squares[square_no]:
-                        squares[square_no].add(board[i][j])
-                    else:
-                        return False
+                if val in squares[square_no] or val in rows[i] or val in cols[j]:
+                    return False
 
-                # rows and cols
-                if board[i][j] != ".":
-                    if board[i][j] not in rows[i]:
-                        rows[i].add(board[i][j])
-                    else:
-                        return False
-
-                    if board[i][j] not in cols[j]:
-                        cols[j].add(board[i][j])
-                    else:
-                        return False
+                rows[i].add(val)
+                cols[j].add(val)
+                squares[square_no].add(val)
 
         return True
