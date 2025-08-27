5496c521e2f4168167007b2d58a905055c8d0fd5
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-27 09:44:27 +0100

    Time: 23 ms (87.39%), Space: 18.6 MB (58.7%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,47 +1,46 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         
-        word_count = {}
+        word_tracker = Counter(words)
 
-        for word in words:
-            if word in word_count:
-                word_count[word] += 1
-            else:
-                word_count[word] = 1
+        res = []
 
-        word_length = len(words[0])
-        indexes = []
+        word_len = len(words[0])
 
-        # this is to offset start position, depending on where word ordering falls in string
-        for i in range(word_length):
-            start = i
+        for i in range(0, word_len):
+            
+            count = 0
+            left = i
             word_window = defaultdict(int)
-            words_used = 0
 
-            for j in range(i,len(s), word_length):
-                curr_word = s[j:j+word_length]
+            for j in range(i,len(s),word_len):
+                curr_word = s[j:j+word_len]
 
-
-                # if we run into a substring that is not a word in words
-                if curr_word not in word_count:
-                    start = j + word_length
+                if curr_word not in word_tracker:
+                    # reset everything
+                    count = 0
+                    left = j + word_len
                     word_window = defaultdict(int)
-                    words_used = 0
                     continue
 
+                # update word_window with current_word
                 word_window[curr_word] += 1
-                words_used += 1
+                count += 1
 
-                while word_window[curr_word] > word_count[curr_word]:
+                # check if the curr_word count exceeds its allowed count
+                while word_window[curr_word] > word_tracker[curr_word]:
+                    left_word = s[left:left+word_len]
+                    word_window[left_word] -= 1
+                    count -= 1
 
-                    word_window[s[start:start+word_length]] -= 1
-                    start += word_length
-                    words_used -= 1
+                    left += word_len
 
-                if words_used == len(words):
-                    indexes.append(start)
+                if count == len(words):
+                    res.append(left)
 
-        return indexes
 
+        return res
+
+                    
