ca037d5e988b31917f63a3c30093f59d43faf143
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-30 13:54:41 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        words = s.split(" ")
+        print(words)
+        if len(words) > len(pattern):
+            return False
+
+        pattern_to_words = {}
+        words_to_pattern = {}
+
+        for i in range(len(pattern)):
+            if (words[i] in words_to_pattern and words_to_pattern[words[i]] != pattern[i]) or (pattern[i] in pattern_to_words and pattern_to_words[pattern[i]] != words[i]):
+                return False
+            
+            pattern_to_words[pattern[i]] = words[i]
+            words_to_pattern[words[i]] = pattern[i]
+
+
+        return True
