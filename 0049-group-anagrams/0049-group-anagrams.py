4dd3ff26f53dfe4dee0687f4cd1d843bfa5d1528
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-30 21:12:06 +0100

    Time: 46 ms (5.62%), Space: 25.4 MB (12.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,23 @@
-from collections import Counter, defaultdict
+from collections import Counter
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        parent_anagram = defaultdict(list)
+        parent_anagram = {}
         res = []
 
         for word in strs:
-            parent_anagram[tuple(sorted(Counter(list(word)).items()))].append(word)
+            letters = list(word)
+
+            anagram = Counter(letters)
+
+            key = tuple(sorted(anagram.items()))
+
+            if key not in parent_anagram:
+                parent_anagram[key] = []
+            
+            parent_anagram[key].append(word)
+                
 
         for key, value in parent_anagram.items():
             res.append(value)
