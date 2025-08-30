9cc592ad6b9b431adff1f4613fe3adabdabbb50e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-30 11:50:45 +0100

    Time: 27 ms (24.07%), Space: 18.1 MB (14.11%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -6,11 +6,20 @@ class Solution:
         if len(magazine) < len(ransomNote):
             return False
         
-        mag_letters = Counter(magazine)
+
+        mag_letters = {}
+        for letter in magazine:
+            if letter not in mag_letters:
+                mag_letters[letter] = 1
+            else:
+                mag_letters[letter] += 1
 
         for char in ransomNote:
-            if mag_letters[char] > 0:
-                mag_letters[char] -= 1
+            if char in mag_letters:
+                if mag_letters[char] > 0:
+                    mag_letters[char] -= 1
+                else:
+                    return False
             else:
                 return False
         
