1feaf99e1d96850ae696edfbbab69acab208b12e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-30 21:18:08 +0100

    Time: 19 ms (36.24%), Space: 22.7 MB (12.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,16 @@
 from collections import defaultdict
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         groups = defaultdict(list)
 
         for word in strs:
+            # Count letters in fixed-size array
             count = [0] * 26
             for c in word:
                 count[ord(c) - ord('a')] += 1
+
+            # Use tuple as hashable key
             groups[tuple(count)].append(word)
 
         return list(groups.values())
