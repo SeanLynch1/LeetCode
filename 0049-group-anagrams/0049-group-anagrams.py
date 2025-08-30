456829743131148334a9f2731649ceca41dfb9f9
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-30 21:12:11 +0100

    Time: 42 ms (6.84%), Space: 25.7 MB (12.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,9 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        parent_anagram = {}
+        parent_anagram = defaultdict(int)
         res = []
 
         for word in strs:
@@ -14,12 +14,9 @@ class Solution:
             key = tuple(sorted(anagram.items()))
 
             if key not in parent_anagram:
-                parent_anagram[key] = []
+                parent_anagram[key] = len(res)
+                res.append([])
+
+            res[parent_anagram[key]].append(word)
             
-            parent_anagram[key].append(word)
-                
-
-        for key, value in parent_anagram.items():
-            res.append(value)
-
         return res
