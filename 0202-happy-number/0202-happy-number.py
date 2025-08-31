4073dc7063a5e38fff03b74f60065f6d2c54aa1f
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-31 13:04:42 +0100

    Time: 0 ms (100%), Space: 17.8 MB (60.94%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -13,12 +13,10 @@ class Solution:
             while num > 0:
                 num, digit = num // 10, num % 10 # gets the first digit, can contain more than one digit, that's why we loop
                 # gets the second digit
-
-                print(num, digit)
                 total += digit ** 2
 
             happy_number = total
-            print(happy_number)
+            
             if happy_number not in found_numbers:
                 found_numbers.add(happy_number)
             else:
