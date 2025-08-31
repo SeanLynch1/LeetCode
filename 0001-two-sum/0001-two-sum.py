a6f6aa70a730412f2f29bac727f12ec9c994f402
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-31 11:35:21 +0100

    Time: 3 ms (50.72%), Space: 18.9 MB (50.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,19 +3,13 @@ from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        nums_pos = defaultdict(list)
+        seen = {}
 
         for i in range(len(nums)):
-            nums_pos[nums[i]].append(i)
 
-        for num in nums:
-            needed = target - num
-            
-            if needed in nums_pos:
-                if num == needed and len(nums_pos[needed]) < 2:
-                    continue
+            needed = target - nums[i]
 
-                idx_2 = nums_pos[needed].pop()
-                idx_1 = nums_pos[num].pop()
+            if needed in seen:
+                return [seen[needed], i]
 
-                return [idx_1, idx_2]      
+            seen[nums[i]] = i
