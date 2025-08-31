510a54dffb23477c67680f7d186bb6145ac4652b
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-31 13:04:22 +0100

    Time: 2 ms (46.23%), Space: 17.7 MB (60.94%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,7 +4,7 @@ class Solution:
         
         happy_number = n
 
-        found_numbers = set()
+        found_numbers = defaultdict(int)
 
         while happy_number != 1:
             digits = list(str(happy_number))
@@ -15,7 +15,7 @@ class Solution:
                 happy_number += (int(digit) ** 2)
             
             if happy_number not in found_numbers:
-                found_numbers.add(happy_number)
+                found_numbers[happy_number] = 1
             else:
                 return False
 
