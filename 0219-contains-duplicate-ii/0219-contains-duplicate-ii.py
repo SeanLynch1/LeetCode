3471f5ac80892d92930a9dd552207598d57e68cf
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-31 22:02:57 +0100

    Time: 41 ms (19.55%), Space: 30.5 MB (93.37%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -4,13 +4,12 @@ class Solution:
         window = set()
 
         for i in range(len(nums)):
-            
             if nums[i] in window:
                 return True
-                
+
             window.add(nums[i])
 
             if len(window) > k:
-                window.remove(nums[i])
+                window.remove(nums[i - k])
 
         return False
