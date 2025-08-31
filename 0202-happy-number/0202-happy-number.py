4a15b35b780496ed21422fc40dc1738ae91bc5f5
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-08-31 12:38:31 +0100

    Time: 7 ms (5.58%), Space: 17.9 MB (18.21%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        happy_number = n
+        found_numbers = set()
+
+        while happy_number != 1:
+            digits = list(str(happy_number))
+
+            happy_number = 0
+
+            for digit in digits:
+                happy_number += (int(digit) ** 2)
+            
+            if happy_number not in found_numbers:
+                found_numbers.add(happy_number)
+            else:
+                return False
+
+            print(happy_number)
+
+        return True
