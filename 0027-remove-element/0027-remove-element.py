0893576909f8b76fedd78c53523409bb6874dbf5
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-02 16:28:37 +0100

    Time: 0 ms (100%), Space: 17.7 MB (82.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,20 @@
 class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        left = 0  # Pointer for where the next non-val element goes
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        left, right = 0, len(nums) - 1
 
-        for right in range(len(nums)):
-            if nums[right] != val:
+        while left <= right:
+            if nums[right] == val:
+                right -= 1
+            elif nums[left] == val:
                 nums[left] = nums[right]
+
+                right -= 1
+                left += 1
+            else:
                 left += 1
 
-        return left  # left is the new length
+
+        return left
+                
+    
