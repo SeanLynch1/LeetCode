a1b0c39af0ccdcefceec701a42c32f69d310e0e3
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-04 12:05:30 +0100

    Time: 44 ms (25.23%), Space: 18.5 MB (87.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,25 +3,17 @@ class Solution:
         
         i = 0
         while i < len(nums) - 1:
-            print("i = ", i)
             if nums[i] == 0:
                 return False
             
             target = nums[i] + i
 
-            print("target = ", target)
-
             for j in range(i + 1, i + nums[i] + 1):
-                print("j = ", j)
 
                 if j >= len(nums) - 1:
                     return True
                 elif nums[j] + j >= target:
                     target = nums[j] + j
                     i = j
-
-                    print("i = ", j, "target = ", nums[j])
-
-                print("\n")
 
         return True
