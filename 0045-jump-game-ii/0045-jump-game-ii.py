3c0fbbe2f4a41ecf57b79fe79b3d99487365d07b
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-04 16:48:35 +0100

    Time: 12 ms (32.43%), Space: 18.9 MB (20.67%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,27 +4,19 @@ class Solution:
         if len(nums) == 1:
             return 0
 
-        i = 0
+        i = 1
         jumps = 1
+        current_jump = 0 + nums[0]
+        print("current_jump = ", current_jump)
 
-        while i < len(nums) - 1:
-            
-            current_jump = i + nums[i] + 1
-
-            if current_jump >= len(nums):
-                break
-            
-            for j in range(i, current_jump):
-                print("j = ", j)
-                if nums[j] + j > current_jump - 1:
-                    current_jump = nums[j] + j + 1
+        while current_jump < len(nums) - 1:
+            for j in range(i, current_jump + 1):
+                if nums[j] + j > current_jump:
+                    current_jump = nums[j] + j
                     i = j
                     
             jumps += 1
-            print("Jumps = ", jumps)
-            print("jumping to index : ", i)
-            print("\n")
+            print("current_jump = ", current_jump)
 
-        print("jumps = ", jumps)
         return jumps
 
