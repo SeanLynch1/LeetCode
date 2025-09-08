0d835e40826824709f72ef4018fa8a76ff5ef65a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 10:41:57 +0100

    Time: 39 ms (19.62%), Space: 23.5 MB (19.18%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,18 +1,28 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
-        balance = 0
-        start = 0
-        gas_tank = 0
+        # O(n)
+        # loop through from first index, if we run out of gas reset everything to zero, begin a 
+        # next index, set this index to the start point, if we keep failing, 
+        # last index will be the solution
+
+        tank = 0
+        total_gas = 0
+        total_cost = 0
+        start_index = 0
 
         for i in range(len(gas)):
-            diff = gas[i] - cost[i]
-            balance += diff
-            gas_tank += diff
+            tank += gas[i]
+            total_cost += cost[i]
+            total_gas += gas[i]
 
-            if gas_tank < 0:
-                start = i + 1
-                gas_tank = 0
-        
-        return start if balance >= 0 else -1
+            if tank >= cost[i]:
+                tank -= cost[i]
+            else:
+                start_index = i + 1
+                tank = 0
 
+        if total_cost <= total_gas:
+            return start_index
+        else:
+            return -1
