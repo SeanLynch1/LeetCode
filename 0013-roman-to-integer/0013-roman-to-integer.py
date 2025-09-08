b959e2ce25bae6f64aea2934ee51a91d3d267fce
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 15:05:14 +0100

    Time: 7 ms (40.21%), Space: 17.8 MB (64.35%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        code = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D" : 500, "M" : 1000}
+
+        letters = list(s)
+        num = 0
+
+        for i in range(len(letters)):
+            val = code[letters[i]]
+
+            if i < len(letters) - 1:
+                if (code[letters[i + 1]] / val) in (5, 10):
+                    val = -val
+            
+            num += val
+        
+        return num
+
