41af43ff4bf81f49ec01f64c38e4fc6fa9dcfa05
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 09:42:45 +0100

    Time: 94 ms (95.07%), Space: 57.3 MB (97.82%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,44 +1,42 @@
-import random
-
 class RandomizedSet:
 
     def __init__(self):
-        # dictionary, used to keep track of number's index
-        self.my_dict = {}
-        # array, used to select random number in O(1)
-        self.my_array = []
+        self.dict_tracker = {}
+        self.list_tracker = []
+        
 
     def insert(self, val: int) -> bool:
-        if val in self.my_dict:
-            return False
-        
-        self.my_dict[val] = len(self.my_array)
-        self.my_array.append(val)
+        if val not in self.dict_tracker:
+            self.dict_tracker[val] = len(self.list_tracker)
+            self.list_tracker.append(val)
 
-        return True
+            return True
+        else:
+            return False
 
     def remove(self, val: int) -> bool:
-        if val not in self.my_dict:
+
+        if val in self.dict_tracker:
+            if val == self.list_tracker[-1]:
+                self.list_tracker.pop()
+                del self.dict_tracker[val]
+                return True
+
+            last_val = self.list_tracker.pop()
+            self.list_tracker[self.dict_tracker[val]] = last_val
+
+            self.dict_tracker[last_val] = self.dict_tracker[val]
+            del self.dict_tracker[val]
+
+            return True
+        else:
             return False
-
-        # find index position
-        index_pos = self.my_dict[val]
-        last_val = self.my_array[-1]
-
-        # update number in array at index_position with last value added
-        self.my_array[index_pos] = last_val
-        self.my_dict[last_val] = index_pos
-
-        # remove last value added
-        self.my_array.pop()
-
-        # update last val in dict to equal array position
-        del self.my_dict[val]
-
-        return True
+        
 
     def getRandom(self) -> int:
-        return random.choice(self.my_array)
+
+        return random.choice(self.list_tracker)
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
