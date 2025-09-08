e088b2a0b888f931c036123b05b14e486d34d805
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 15:09:35 +0100

    Time: 6 ms (45.55%), Space: 17.8 MB (64.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,11 +4,12 @@ class Solution:
         code = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D" : 500, "M" : 1000}
 
         num = 0
+        n = len(s)
 
-        for i in range(len(s)):
+        for i in range(n):
             val = code[s[i]]
 
-            if i < len(s) - 1:
+            if i < n - 1:
                 if (code[s[i + 1]] / val) % 5 == 0:
                     val = -val
             
