93833daf4e1306caa991c3e4f8bc3563ee7ffb2e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 15:43:22 +0100

    Time: 4 ms (64.47%), Space: 17.6 MB (98.84%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,7 +1,7 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         
-        code = {1000 : "M", 900: "CM", 500 : "D", 400 : "CD", 100 : "C", 90 :"XC", 50 : "L", 40 : "XL", 10 : "X", 9 : "IX", 8 : "VIII", 7 : "VII", 6 : "VI", 5 : "V", 4 : "IV", 3 : "III", 2 : "II", 1 : "I" }
+        code = {1000 : "M", 900: "CM", 500 : "D", 400 : "CD", 100 : "C", 90 :"XC", 50 : "L", 40 : "XL", 10 : "X", 9 : "IX", 5 : "V", 4 : "IV", 1 : "I" }
 
         numeral = []
 
