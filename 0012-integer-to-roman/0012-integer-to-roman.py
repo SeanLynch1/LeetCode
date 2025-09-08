e6b2a343ccadb64fb623d2d8fe6f72b6d7c8df00
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 15:40:50 +0100

    Time: 14 ms (8.39%), Space: 17.9 MB (44.4%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        code = {1000 : "M", 900: "CM", 500 : "D", 400 : "CD", 100 : "C", 90 :"XC", 50 : "L", 40 : "XL", 10 : "X", 9 : "IX", 8 : "VIII", 7 : "VII", 6 : "VI", 5 : "V", 4 : "IV", 3 : "III", 2 : "II", 1 : "I" }
+
+        numeral = []
+
+        for key, value in code.items():
+
+            while num >= key:
+                numeral.append(value)
+                num -= key
+
+        return ''.join(numeral)
+
