de56fd58820a73f65b6903d97d5ecfc7c2367ab3
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 15:12:30 +0100

    Time: 19 ms (5.51%), Space: 18 MB (20.26%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -5,15 +5,19 @@ class Solution:
 
         num = 0
         n = len(s)
+        i = 0
 
-        for i in range(n):
+        while i < n:
             val = code[s[i]]
 
             if i < n - 1:
                 if (code[s[i + 1]] / val) % 5 == 0:
-                    val = -val
+                    val = code[s[i + 1]] - val
+                    i += 1
             
+            print("i = , ",i ,val)
             num += val
+            i += 1
         
         return num
 
