b8c8747ea98fc73de83e297670e7c5318408f803
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 16:21:45 +0100

    Time: 1 ms (35.56%), Space: 17.6 MB (95.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,7 +4,6 @@ class Solution:
         prefix = strs[0]
 
         for s in strs:
-
             i = 0
             while i < len(s) and i <len(prefix) and prefix[i] == s[i]:
                 i += 1
