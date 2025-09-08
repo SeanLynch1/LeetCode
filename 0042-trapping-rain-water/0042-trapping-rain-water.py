95e74d1dc29ff9beaf06882ba2584ab6d9b2088a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 13:16:13 +0100

    Time: 91 ms (5.05%), Space: 19.3 MB (38.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,36 +1,52 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
         
-        water = 0
-        water_bucket = 0
         left = 0
         right = len(height) - 1
-        left_peak = height[left]
-        right_peak = height[right]
 
-        # loop both directions
-        while right > left:
-            # find lowest side
+        bucket = 0
+        water = 0
+
+        l_height = height[left]
+        r_height = height[right]
+
+        print("l_height = ", l_height)
+        print("r_height = ", r_height)
+
+        while left < right:
+            
             if height[left] < height[right]:
+                
+                if height[left] > l_height:
+                    water += bucket
+                    print(f"emptying bucket full of {bucket} to water, water = {water}")
 
+                    l_height = height[left]
+                    bucket = 0
+                    print("upgrading l_height to ", l_height)
+                else:
+                    bucket += l_height - height[left]
+                    print(f"filling bucket, bucket = {bucket}")
+
+                print("l_height = ", l_height)
+                print("\n")
                 left += 1
-
-                if left_peak < height[left]:
-                    left_peak = height[left]
-
-                diff = left_peak - height[left]
-                water += diff
-
             else:
+                if height[right] > r_height:
+                    water += bucket
+                    print(f"emptying bucket full of {bucket} to water, water = {water}")
+
+                    r_height = height[right]
+                    bucket = 0
+                    print("upgrading r_height to ", r_height)
+
+                else:
+                    bucket += r_height - height[right]
+                    print(f"filling bucket, bucket = {bucket}")
+
+
+                print("r_height = ", r_height)
                 right -= 1
-
-                if right_peak < height[right]:
-                    right_peak = height[right]
-
-                diff = right_peak - height[right]
-                water += diff
-
-
+        water += bucket
+        
         return water
