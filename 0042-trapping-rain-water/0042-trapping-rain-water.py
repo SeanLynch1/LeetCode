6a3250503639114867b5d535ccfef8ab7b03d3c0
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 13:18:32 +0100

    Time: 3 ms (97.29%), Space: 19.3 MB (69.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -10,43 +10,24 @@ class Solution:
         l_height = height[left]
         r_height = height[right]
 
-        print("l_height = ", l_height)
-        print("r_height = ", r_height)
-
         while left < right:
-            
             if height[left] < height[right]:
                 
                 if height[left] > l_height:
                     water += bucket
-                    print(f"emptying bucket full of {bucket} to water, water = {water}")
-
                     l_height = height[left]
                     bucket = 0
-                    print("upgrading l_height to ", l_height)
                 else:
                     bucket += l_height - height[left]
-                    print(f"filling bucket, bucket = {bucket}")
 
-                print("l_height = ", l_height)
-                print("\n")
                 left += 1
             else:
                 if height[right] > r_height:
                     water += bucket
-                    print(f"emptying bucket full of {bucket} to water, water = {water}")
-
                     r_height = height[right]
                     bucket = 0
-                    print("upgrading r_height to ", r_height)
-
                 else:
                     bucket += r_height - height[right]
-                    print(f"filling bucket, bucket = {bucket}")
-
-
-                print("r_height = ", r_height)
                 right -= 1
-        water += bucket
-        
-        return water
+
+        return water + bucket
