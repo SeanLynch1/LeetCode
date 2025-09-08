a95d502d7a4a838da6b485729e8af6d30a3bcb08
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-08 12:01:10 +0100

    Time: 10 ms (92.05%), Space: 19.8 MB (68.89%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,27 +1,20 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        
         candies = [1] * len(ratings)
         count = 0
-
         # forward pass
         for i in range(1, len(ratings)):
             
             if ratings[i] > ratings[i-1]:
                 candies[i] = candies[i - 1] + 1
 
-
-        print(candies)
         count += candies[-1]
         
         # backwards pass
         for j in range(len(ratings) - 2, -1, -1):
-            
             if ratings[j] > ratings[j + 1] and candies[j] <= candies[j + 1]:
                 candies[j] = candies[j + 1] + 1
             
             count += candies[j]
-
-        print(candies)
 
         return count
