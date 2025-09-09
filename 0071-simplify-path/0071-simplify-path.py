2accde88da341ff4bc1079946089ea0e9742c035
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-09 23:51:02 +0100

    Time: 0 ms (100%), Space: 17.9 MB (54.49%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -7,15 +7,10 @@ class Solution:
 
         for i in range(len(path)):
             text = path[i]
-            if simplified_path and simplified_path[-1] == "..":
-                    simplified_path.pop()
 
             if text == '..':
                 if len(simplified_path) > 1:
                     simplified_path.pop()
-
-                if i < len(path) - 1:
-                    simplified_path.append(text)
 
             elif text != '' and text != '.':
                 simplified_path.append(text)
