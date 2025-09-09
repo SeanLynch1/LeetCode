4d75c4d4d56eea09c433149bc5427edf8e92a359
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-09 23:51:44 +0100

    Time: 3 ms (36.79%), Space: 18 MB (34.54%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -5,16 +5,16 @@ class Solution:
 
         path = path.split('/')
 
+        print(path)
         for i in range(len(path)):
             text = path[i]
 
             if text == '..':
                 if len(simplified_path) > 1:
                     simplified_path.pop()
-
             elif text != '' and text != '.':
                 simplified_path.append(text)
-            
+        print(simplified_path)
         if len(simplified_path) <= 1:
             return '/'
         else:
