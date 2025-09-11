5a7717fc916c574348f975a3fa393a4aff2f28b1
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-11 14:51:25 +0100

    Time: 3 ms (63.75%), Space: 19.1 MB (88.08%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -8,17 +8,16 @@ class Solution:
             if token not in ops:
                 stack.append(int(token))
             elif len(stack) > 1:
+                val1 = stack.pop()
+                val2 = stack.pop()
+
                 if token == "+":
-                    stack.append(stack.pop() + stack.pop())
+                    stack.append(val2 + val1)
                 elif token == "-":
-                    val1 = stack.pop()
-                    val2 = stack.pop()
                     stack.append(val2 - val1)
                 elif token == "*":
-                    stack.append(stack.pop() * stack.pop())
+                    stack.append(val2 * val1)
                 else:
-                    val1 = stack.pop()
-                    val2 = stack.pop()
                     stack.append(int(val2 / val1))
 
         return stack[0]
