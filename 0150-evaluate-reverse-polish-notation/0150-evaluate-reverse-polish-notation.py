7d954748ad690f4fcbfa0279d2cc9225adb2776c
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-11 14:49:30 +0100

    Time: 3 ms (63.75%), Space: 19 MB (97.75%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,24 +1,25 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
+        ops = {"*","-","+","/"}
 
         for token in tokens:
-            try:
+
+            if token not in ops:
                 token = int(token)
                 stack.append(token)
-            except:
-                if len(stack) > 1:
-                    if token == "+":
-                        stack.append(stack.pop() + stack.pop())
-                    elif token == "-":
-                        val1 = stack.pop()
-                        val2 = stack.pop()
-                        stack.append(val2 - val1)
-                    elif token == "*":
-                        stack.append(stack.pop() * stack.pop())
-                    else:
-                        val1 = stack.pop()
-                        val2 = stack.pop()
-                        stack.append(int(val2 / val1))
+            elif len(stack) > 1:
+                if token == "+":
+                    stack.append(stack.pop() + stack.pop())
+                elif token == "-":
+                    val1 = stack.pop()
+                    val2 = stack.pop()
+                    stack.append(val2 - val1)
+                elif token == "*":
+                    stack.append(stack.pop() * stack.pop())
+                else:
+                    val1 = stack.pop()
+                    val2 = stack.pop()
+                    stack.append(int(val2 / val1))
 
         return stack[-1]
