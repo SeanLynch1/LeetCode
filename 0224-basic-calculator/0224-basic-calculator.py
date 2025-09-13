fd4aa762275aa1068e6542df00835f9329dffa9c
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-13 17:06:49 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,38 +1,38 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        result = 0   # current accumulated result
-        number = 0   # current number being built
-        sign = 1     # +1 for positive, -1 for negative
+        stack = [] # stores number inside brackets that haven't been finished yet, used to update result
+        result = 0 # the final result, is updated when a bracket is closed or at the return
+        sign = 1 # the sign before the bracket, that the number inside will be multiplied by
+        number = 0 # the temporary numbre in brackets that will be added to result once bracket is closed
 
         for char in s:
-            if char.isdigit():
-                number = number * 10 + int(char)  # build multi-digit numbers
 
-            elif char == '+':
-                result += sign * number
+            if char.isdigit():
+                # for numbers next to each other '123', 1, then 10 + 2, then 120 + 3
+                number = number * 10 + int(char)
+            elif char == "+":
+                # update result
+                result += number * sign
                 number = 0
                 sign = 1
-
-            elif char == '-':
-                result += sign * number
+            elif char == "-":
+                result += number * sign
                 number = 0
                 sign = -1
-
-            elif char == '(':
-                # push the result and sign before the parenthesis
+            elif char == ")":
+                result += number * sign
+                result *= stack.pop()
+                result += stack.pop()
+                number = 0
+                sign = 1
+            elif char == "(":
                 stack.append(result)
                 stack.append(sign)
-                # reset for new sub-expression
                 result = 0
                 sign = 1
 
-            elif char == ')':
-                result += sign * number
-                number = 0
-                result *= stack.pop()  # stack.pop() = sign before '('
-                result += stack.pop()  # stack.pop() = result calculated before '('
+            print("char = ", char)
+            print(f"result = {result}")
+            print(stack, "\n")
 
-            # ignore spaces
-
-        return result + sign * number
+        return result + (number * sign)
