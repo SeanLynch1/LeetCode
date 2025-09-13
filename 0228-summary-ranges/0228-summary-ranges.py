1851576e6cae3643ff6ddf3e09ded03fa342ca3a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-13 17:26:05 +0100

    Time: 0 ms (100%), Space: 17.9 MB (33.53%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,26 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        start = nums[0]
+        res = []
+
+        for i in range(len(nums)):
+            
+            if i < len(nums) - 1:
+                if (nums[i] + 1) != nums[i + 1]:
+                    if start == nums[i]:
+                        res.append(str(nums[i]))
+                    else:
+                        res.append(f"{start}->{nums[i]}")
+
+                    start = nums[i + 1]
+            else:
+                if start == nums[i]:
+                    res.append(str(nums[i]))
+                else:
+                    res.append(f"{start}->{nums[i]}")
+
+        
+        return res
