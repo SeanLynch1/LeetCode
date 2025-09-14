5f2d0e5bc1f8f44b13f181448ac5176cfbc41763
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-14 20:16:50 +0100

    Time: 11 ms (29.81%), Space: 21.8 MB (15.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -8,16 +8,13 @@ class Solution:
         res = []
 
         while i < n:
-            temp = intervals[i]
             j = i
             max_int = intervals[i][1]
             while j < n - 1 and max_int >= intervals[j + 1][0]:
-                temp.extend(intervals[j + 1])
                 max_int = max(max_int, intervals[j + 1][1])
                 j += 1
             
-            temp = sorted(temp)
-            res.append([temp[0], temp[-1]])
+            res.append([intervals[i][0], max_int])
             i = j + 1
 
         return res
