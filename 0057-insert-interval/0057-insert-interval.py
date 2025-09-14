a5f81dd942611cb51543598149f75daccae7f124
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-14 20:50:49 +0100

    Time: 0 ms (100%), Space: 19.7 MB (40.32%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -8,15 +8,15 @@ class Solution:
         max_int = intervals[0][1]
 
         res = []
-        for i in range(len(intervals) - 1):
+        for i in range(1, len(intervals)):
 
-            if max_int >= intervals[i + 1][0]:
-                max_int = max(max_int, intervals[i + 1][1])
+            if max_int >= intervals[i][0]:
+                max_int = max(max_int, intervals[i][1])
             else:
                 res.append([min_int, max_int])
 
-                min_int = intervals[i + 1][0]
-                max_int = intervals[i + 1][1]
+                min_int = intervals[i][0]
+                max_int = intervals[i][1]
 
         res.append([min_int, max_int])
 
