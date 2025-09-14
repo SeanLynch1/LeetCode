030f4e34ce6d87b55f29d6d3a4a5a61dc5ba7f72
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-14 21:23:30 +0100

    Time: 103 ms (47.4%), Space: 51.5 MB (7.83%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -4,7 +4,7 @@ class Solution:
             return 0
 
         arrows = 1
-        points.sort(key= lambda x : x[0])
+        points.sort(key= lambda x : x[1])
         aligned = [points[0]]
 
         for i in range(1, len(points)):
