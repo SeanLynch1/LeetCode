ff7b8aa4ad0d8201d5d8044e7f48f8d1ed2b443f
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-15 23:42:42 +0100

    Time: 51 ms (39.36%), Space: 20.6 MB (5.79%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -11,19 +11,15 @@ class Solution:
             return False
 
         curr = head
-        print(curr.val)
-
         count = 0
+
         while curr:
             curr.seen = True
             curr = curr.next
 
-            if curr:
-                print(curr.val)
-            
             count += 1
 
-            if count >= 10001:
+            if count > 10000:
                 return True
 
         return False
