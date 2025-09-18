723f250797bd77c9da1c612fbf2d3e9207a6b97a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-09-18 23:44:47 +0100

    Time: 36 ms (86.3%), Space: 18.4 MB (84.82%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -1,37 +1,24 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
-"""
-
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
-            return head
+            return None
 
-        indexes = [Node(head.val)]
-        randoms = {head : 0}
+        # map original -> copy
+        mapping = {}
 
-
-        curr = head.next
-        idx = 1
-
+        # First pass: create all new nodes
+        curr = head
         while curr:
-            randoms[curr] = idx
-
-            new = Node(curr.val)
-            indexes[-1].next = new
-            indexes.append(new)
-
+            mapping[curr] = Node(curr.val)
             curr = curr.next
-            idx += 1
 
-        for i, node in enumerate(randoms):
-            if node.random in randoms:
-                node.random = randoms[node.random]
-                indexes[i].random = indexes[node.random]     
+        # Second pass: assign next and random pointers
+        curr = head
+        while curr:
+            if curr.next:
+                mapping[curr].next = mapping[curr.next]
+            if curr.random:
+                mapping[curr].random = mapping[curr.random]
+            curr = curr.next
 
-        return indexes[0]
+        return mapping[head]
