8f0baedf8e7aaa851cf8d7138c2764dc20867514
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-02 22:41:06 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,50 +1,108 @@
-class DoublyNode:
-    def __init__(self, val, key):
+class ListNode:
+    def __init__(self,key,value):
+        self.key = key
+        self.val = value
         self.next = None
         self.prev = None
-        self.val = val
-        self.key = key
-
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.nodes_in_list = 0
-        self.lru = DoublyNode(-1, -1)
-        self.mru = DoublyNode(-1, -1)
-        self.lru.next = self.mru
-        self.mru.prev = self.lru
-        self.hash = {}
+        self.my_dict = {}
+        self.head = ListNode(0,0)
+        self.tail = ListNode(0,0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+        temp = self.head
+
+
+    def remove(self, key: int):
+        print(f"removing key, {key}")
+        prev, next = self.my_dict[key].prev, self.my_dict[key].next
+        prev.next = next
+        next.prev = prev
+
+        del self.my_dict[key]
+        
+        
+
+    def insert(self, key: int):
+        prev, next = self.head, self.head.next
+
+        print(f"inserting {key}")
+        
+        next.prev = self.my_dict[key]
+        self.head.next = self.my_dict[key]
+
+        self.my_dict[key].prev = prev
+        self.my_dict[key].next = next
+        
 
     def get(self, key: int) -> int:
-        if key in self.hash:
-            self.remove(self.hash[key])
-            self.append(self.hash[key])
-            return self.hash[key].val
+        
+        print(f"getting key, {key}")
+        if key not in self.my_dict:
 
-        return -1
+            print(f"key, {key} not in self.my_dict")
+            print("\n")
+            return -1
+        else:
+            value = self.my_dict[key].val
+            self.remove(key)
 
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+            self.my_dict[key] = ListNode(key, value)
+            self.insert(key)
 
-    def append(self, node):
-        node.next = self.mru
-        node.prev = self.mru.prev
-        self.mru.prev.next = node
-        self.mru.prev = node
+            temp = self.head.next
+
+            print("linked list = ")
+            while temp.next:
+                print(f"key = {temp.key}, val = {temp.val}, prev = {temp.prev.key}, next = {temp.next.key}")
+                temp = temp.next
+
+            print()
+            print("dictionary status = ")
+            for key, value in self.my_dict.items():
+                print(f"key = {key}, val = {value.val}")
+            
+            print("\n")
+
+            return self.my_dict[key].val
+
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hash:
-            self.remove(self.hash[key])
-            self.append(self.hash[key])
-            self.hash[key].val = value
+
+        if key not in self.my_dict:
+            self.my_dict[key] = ListNode(key, value)
+            self.insert(key)
+
+            if len(self.my_dict) > self.capacity:
+                self.remove(self.tail.prev.key)
         else:
-            self.nodes_in_list += 1
-            if self.nodes_in_list > self.capacity:
-                self.nodes_in_list -= 1
-                del self.hash[self.lru.next.key]
-                self.remove(self.lru.next)
-            node = DoublyNode(value, key)
-            self.hash[key] = node
-            self.append(node)
+            self.remove(key)
+            self.my_dict[key] = ListNode(key, value)
+            self.insert(key)
+
+        temp = self.head.next
+
+        print("linked list = ")
+        while temp.next:
+            print(f"key = {temp.key}, val = {temp.val}, prev = {temp.prev.key}, next = {temp.next.key}")
+            temp = temp.next
+
+        print()
+        print("dictionary status = ")
+        for key, value in self.my_dict.items():
+            print(f"key = {key}, val = {value.val}")
+        
+        print("\n")
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
