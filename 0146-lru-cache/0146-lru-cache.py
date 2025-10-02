243f67d1f493daa8c78b1a9e392cdb56a182517e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-02 22:56:00 +0100

    Time: 168 ms (24.87%), Space: 78.6 MB (12.62%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -15,48 +15,55 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def remove(self, key: int):
-        prev, next = self.my_dict[key].prev, self.my_dict[key].next
+    def remove(self, node: ListNode):
+        prev, next = node.prev, node.next
         prev.next = next
         next.prev = prev
 
-        del self.my_dict[key]
+        del self.my_dict[node.key]
         
-    def insert(self, key: int):
+    def insert(self, node: ListNode):
         prev, next = self.head, self.head.next
 
-        
-        next.prev = self.my_dict[key]
-        self.head.next = self.my_dict[key]
+        next.prev = node
+        self.head.next = node
 
-        self.my_dict[key].prev = prev
-        self.my_dict[key].next = next
+        node.prev = prev
+        node.next = next
 
     def get(self, key: int) -> int:
         
         if key not in self.my_dict:
             return -1
         else:
-            value = self.my_dict[key].val
-            self.remove(key)
+            node = self.my_dict[key]
+            key = node.key
+            value = node.val
 
-            self.my_dict[key] = ListNode(key, value)
-            self.insert(key)
+            self.remove(node)
 
-            return self.my_dict[key].val
+            node = ListNode(key, value)
+            self.my_dict[key] = node
+            self.insert(node)
+
+            return value
 
     def put(self, key: int, value: int) -> None:
 
         if key not in self.my_dict:
-            self.my_dict[key] = ListNode(key, value)
-            self.insert(key)
+            node = ListNode(key, value)
+            self.my_dict[key] = node
+            self.insert(node)
 
             if len(self.my_dict) > self.capacity:
-                self.remove(self.tail.prev.key)
+                self.remove(self.tail.prev)
         else:
-            self.remove(key)
-            self.my_dict[key] = ListNode(key, value)
-            self.insert(key)
+            node = self.my_dict[key]
+            self.remove(node)
+
+            node = ListNode(key, value)
+            self.my_dict[key] = node
+            self.insert(node)
 
 
 # Your LRUCache object will be instantiated and called as such:
