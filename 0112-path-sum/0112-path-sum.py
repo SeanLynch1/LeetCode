7e58bc829c5b4ebb6584462db697929bbbaf36ca
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-11 12:05:53 +0100

    Time: 0 ms (100%), Space: 18.8 MB (69.43%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,29 +6,13 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        self.found = False
+        if not root:
+            return False
 
-        def helper(node: Optional[TreeNode], curr_sum: int) -> bool:
+        # Leaf node check
+        if not root.left and not root.right:
+            return targetSum == root.val
 
-            if self.found:
-                return True
-
-            if not node:
-                return False
-
-            curr_sum += node.val
-
-            left = helper(node.left, curr_sum)
-            right = helper(node.right,curr_sum)
-
-            # found leaf node
-            if not left and not right:
-                if curr_sum == targetSum:
-                    self.found = True
-            
-            return True
-
-        helper(root, 0)
-
-        return self.found
+        remaining = targetSum - root.val
+        return (self.hasPathSum(root.left, remaining) or 
+                self.hasPathSum(root.right, remaining))
