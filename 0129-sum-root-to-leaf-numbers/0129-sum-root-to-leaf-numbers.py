3220d7c3325620301900a3cdcaebbaef52f90877
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-11 13:30:09 +0100

    Time: 0 ms (100%), Space: 18 MB (15.37%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,8 +6,29 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        total = 0
+        # Recursive
+
+
+        def dfs(node, curr_sum):
+
+            if not node:
+                return 0
+            
+            curr_sum = (curr_sum * 10) + node.val
+            print(f"curr_sum = {curr_sum}")
+
+            if not node.left and not node.right:
+                return curr_sum
+
+            return dfs(node.left, curr_sum) + dfs(node.right, curr_sum)
+
+
+        return dfs(root, 0)
+
+
+
+        # Iterative
+        '''total = 0
         stack = [(root, str(root.val))]
 
         while stack:
@@ -23,6 +44,6 @@ class Solution:
                 stack.append((node.right, string_val + str(node.right.val)))
 
 
-        return total
+        return total'''
 
         
