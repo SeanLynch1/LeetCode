173fa1b04eb3e10dd9d2e0eaf98c05d2e1ecc438
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-12 12:21:04 +0100

    Time: 102 ms (5.34%), Space: 23 MB (79.39%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        # post order -> left right node
+        # 9, 15, 7, 20, -10
+
+        self.optimal_sum = root.val
+
+        def helper(node: Optional[TreeNode]) -> int:
+            
+            if not node:
+                return 0
+
+            self.optimal_sum = max(node.val, self.optimal_sum)
+
+            left_side = helper(node.left)
+            right_side = helper(node.right)
+
+            combined = node.val + left_side + right_side
+            max_side = max(max(left_side + node.val, right_side + node.val), node.val)
+            largest = max(combined, max_side)
+            self.optimal_sum = max(largest, self.optimal_sum)
+
+            print(f"node.val = {node.val}")
+            print(f"left_side = {left_side}")
+            print(f"right_side = {right_side}")
+            print(f"optimal_sum = {self.optimal_sum}")
+            print(f"right_side sum = {right_side + node.val}")
+            print(f"left_sum sum = {left_side + node.val}")
+
+
+            print("\n")
+
+            return max_side
+        
+        final = helper(root)
+        print("final optimal_sum = ", self.optimal_sum)
+        print("final = ", final)
+        return max(final, self.optimal_sum)
