61a601f87d0468f9c1b9b781a8be799638fad066
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-12 12:26:59 +0100

    Time: 17 ms (48.91%), Space: 22.9 MB (79.39%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -17,14 +17,14 @@ class Solution:
             if not node:
                 return 0
 
-            self.optimal_sum = max(node.val, self.optimal_sum)
-
             left_side = max(helper(node.left), 0)
             right_side = max(helper(node.right), 0)
 
             combined = node.val + left_side + right_side
             max_side = max(left_side + node.val, right_side + node.val)
             largest = max(combined, max_side)
+
+            
             self.optimal_sum = max(largest, self.optimal_sum)
 
             return max_side
