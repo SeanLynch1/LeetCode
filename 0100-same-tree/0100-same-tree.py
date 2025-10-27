c0eadfdc3786ec8d0b024a109b8a3c1fca780f96
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-27 14:35:36 +0000

    Time: 0 ms (100%), Space: 18 MB (21.13%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,18 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        stack = [(p, q)]
+        
+        stack = []
+        curr_p = p
+        curr_q = q
 
-        while stack:
-            n1, n2 = stack.pop()
+        while stack or curr_p or curr_q:
+            while curr_p or curr_q:
 
-            if not n1 and not n2:
-                continue
-            if not n1 or not n2:
-                return False
-            if n1.val != n2.val:
-                return False
+                if not (curr_p and curr_q) or curr_p.val != curr_q.val:
+                    return False
+                
+                stack.append(curr_p)
+                stack.append(curr_q)
 
-            stack.append((n1.left, n2.left))
-            stack.append((n1.right, n2.right))
+                curr_p = curr_p.left
+                curr_q = curr_q.left
+
+            curr_q = stack.pop().right
+            curr_p = stack.pop().right
+
+
 
         return True
+
+            
