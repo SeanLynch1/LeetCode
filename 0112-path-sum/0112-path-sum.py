352268557dd79a4bc14005720b78a68c87deb53c
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-10-30 22:58:50 +0000

    Time: 0 ms (100%), Space: 18.9 MB (69.22%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -15,7 +15,4 @@ class Solution:
         if not root.left and not root.right and curr_sum == 0:
             return True
 
-        if self.hasPathSum(root.left, curr_sum) or self.hasPathSum(root.right, curr_sum):
-            return True
-
-        return False
+        return self.hasPathSum(root.left, curr_sum) or self.hasPathSum(root.right, curr_sum)
