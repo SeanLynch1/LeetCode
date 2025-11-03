7da651d333a426abe976c6d6fdfa0788eef8fdbb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-03 16:16:39 +0000

    Time: 3 ms (57%), Space: 23.4 MB (38.2%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,29 +6,32 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        count = 0
         curr = root
-        
+        count = 0
+
         while curr:
+
             left, right = curr.left, curr.right
-            left_depth = self._depth(left)
-            right_depth = self._depth(right)
-            
+            left_depth = self.count_depth(left)
+            right_depth = self.count_depth(right)
+
+            print(f"left_depth = {left_depth}, right_depth = {right_depth}")
+            # if right side is possibly imperfect
             if left_depth == right_depth:
-                # Left subtree is perfect
-                count += 1 << left_depth  # 2^left_depth
-                curr = curr.right
-            else:
-                # Right subtree is perfect
-                count += 1 << right_depth  # 2^right_depth
-                curr = curr.left
                 
+                count += (2 ** left_depth)
+                curr = curr.right
+            # if left side is possibly imperfect
+            else:
+                count += (2 ** right_depth)
+                curr = curr.left
+
         return count
 
-    def _depth(self, node):
-        depth = 0
+    def count_depth(self, node: TreeNode) -> int:
+        num = 0
         while node:
-            depth += 1
+            num += 1
             node = node.left
-        return depth
-
+        
+        return num
