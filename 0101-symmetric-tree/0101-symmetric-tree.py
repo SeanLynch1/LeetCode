d0ffdb2d1b401455c67daf07ad824542deb38b96
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-04 14:49:16 +0000

    Time: 0 ms (100%), Space: 17.8 MB (83.58%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,30 +6,8 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        # iterative
-        stack = [(root.left, root.right)]
-
-        while stack:
-            curr_left, curr_right = stack.pop()
-
-            if not curr_left and not curr_right:
-                continue
-            elif not curr_left or not curr_right:
-                return False
-            elif curr_left.val != curr_right.val:
-                return False
-
-            stack.append((curr_left.right, curr_right.left))
-            stack.append((curr_left.left, curr_right.right))
-
         
-        return True
-
-
-
-
-        # recursive
-        '''def helper(left_node, right_node):
+        def helper(left_node, right_node):
             if not left_node and not right_node:
                 return True
             elif not left_node or not right_node:
@@ -37,16 +15,6 @@ class Solution:
             elif left_node.val != right_node.val:
                 return False
 
-            if not helper(left_node.left, right_node.right):
-                return False
+            return (helper(left_node.left, right_node.right) and helper(left_node.right, right_node.left))
 
-            if not helper(left_node.right, right_node.left):
-                return False
-
-            return True
-        
-        return helper(root.left, root.right)'''
-
-
-
-        
+        return helper(root.left, root.right)
