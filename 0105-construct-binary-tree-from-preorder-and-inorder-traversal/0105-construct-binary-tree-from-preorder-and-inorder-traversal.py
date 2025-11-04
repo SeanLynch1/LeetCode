e6ff6f70d9362fced9e0a8790832d6344af9bef4
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-04 15:03:53 +0000

    Time: 2 ms (90.16%), Space: 19 MB (92.4%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,34 +7,21 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         
-        # preorder -> node left right
-        # inorder -> left node right
+        self.inorder_mapping = {val: idx for idx, val in enumerate(inorder)}
+        self.next_val = 0
 
-        # since preorder records each node straight away, iterate through it, checking left in inorder of the next in preorder gives you what's left of the tree, checking right of the next in preorder gives you what's right of the tree
-
-        self.inorder_map = {}
-        self.next_pre = 0
-
-        for idx, val in enumerate(inorder):
-            self.inorder_map[val] = idx
-
-        def helper(left,right) -> TreeNode:
-            if left == right:
+        def helper(left, right) -> TreeNode:
+           if left >= right:
                 return None
 
-            val = preorder[self.next_pre]
-            self.next_pre += 1
+           node = TreeNode(preorder[self.next_val])
 
-            node = TreeNode(val)
-            mid = self.inorder_map[val]
+           mid = self.inorder_mapping[node.val]
+           self.next_val += 1
 
-            # for the next iteration
+           node.left = helper(left, mid)
+           node.right = helper(mid + 1, right)
 
-            # next value is to the left of current
-            node.left = helper(left, mid)
-            # value is the right of current
-            node.right = helper(mid + 1, right)
+           return node
 
-            return node
-
-        return helper(0,len(inorder))
+        return helper(0, len(preorder))
