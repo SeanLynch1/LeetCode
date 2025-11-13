42f81396b7d7e61c54b494b0227cb86601b9ae19
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-13 09:32:52 +0000

    Time: 3 ms (86.35%), Space: 21.7 MB (57.09%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,36 +3,43 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        
         no_rows = len(board)
         no_cols = len(board[0])
 
-        # change all o's found to e
-        def helper(row: int, col: int):
+
+        def helper(row: int, col: int) -> None:
+            
             if (row < 0 or row == no_rows) or (col < 0 or col == no_cols) or board[row][col] != "O":
                 return
 
+            print("hello?")
             board[row][col] = "E"
-            # check down
+            # down
             helper(row + 1, col)
-            # check left
+            # left
             helper(row, col - 1)
-            # check right
+            # right
             helper(row, col + 1)
-            # check up
+            # up
             helper(row - 1, col)
+
             return
 
-        # start from top and bottom row
-        for r in [0, no_rows - 1]:
+        # start at edge tiles
+        for r in [0,no_rows-1]:
             for c in range(no_cols):
+                print(board[r][c])
                 if board[r][c] == "O":
+                    print("Hi")
                     helper(r, c)
 
-        # start from left and right side
         for c in [0, no_cols - 1]:
             for r in range(no_rows):
                 if board[r][c] == "O":
-                    helper(r, c)
+                    helper(r,c)
+        for i in range(no_rows):
+            print(board[i])
 
         for r in range(no_rows):
             for c in range(no_cols):
@@ -41,3 +48,5 @@ class Solution:
                 elif board[r][c] == "O":
                     board[r][c] = "X"
 
+
+                
