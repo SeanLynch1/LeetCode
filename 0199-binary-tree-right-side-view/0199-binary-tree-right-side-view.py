2ef5ba322ce87e4526402feae8e38b6316b74b44
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-13 12:12:45 +0000

    Time: 0 ms (100%), Space: 17.8 MB (37.8%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,52 +6,26 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
-        '''
-        # DFS:
-        if not root:
-            return []
-        output = []
-        stack = [(root, 1)]
-
-        while stack:
-            curr, depth = stack.pop()
-
-            if depth > len(output):
-                output.append(curr.val)
-
-            if curr.left:
-                stack.append((curr.left, depth + 1))
-            if curr.right:
-                stack.append((curr.right, depth + 1))
-            
-
-        return output
-        '''
-
-        # BFS:
         if not root:
             return []
             
-        output = []
         queue = deque([root])
+        output = []
 
         while queue:
-
-            no_nodes_in_level = len(queue)
-
-            for i in range(no_nodes_in_level):
-                
-                # keep poping the left most node until we reach the final one
-                # we need to iterate through each node, to add their children for the next layer
+            for i in range(len(queue)):
                 curr = queue.popleft()
-                print(f"curr = {curr}")
-                if i == no_nodes_in_level - 1:
-                    output.append(curr.val)
 
                 if curr.left:
                     queue.append(curr.left)
+
                 if curr.right:
                     queue.append(curr.right)
-        
+            
+            output.append(curr.val)
+
         return output
+
+            
+
+            
