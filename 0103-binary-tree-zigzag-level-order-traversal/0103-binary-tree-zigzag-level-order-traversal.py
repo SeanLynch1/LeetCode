492a0f940242e032f951ab279d926de9183218c7
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-17 21:26:46 +0000

    Time: 3 ms (6.55%), Space: 18 MB (76.85%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -8,42 +8,34 @@ class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
+
         queue = deque([root])
         output = []
         toggle = False
 
         while queue:
-            level_nodes = []
-            print(f"queue = {queue}")
+            temp = []
             for i in range(len(queue)):
+                if not toggle:
+                    curr = queue.popleft()
 
-                if toggle:
+                    if curr.left:
+                        queue.append(curr.left)
+                    
+                    if curr.right:
+                        queue.append(curr.right)
+                else:
                     curr = queue.pop()
-                    print(f"curr = {curr.val}")
-                    level_nodes.append(curr.val)
 
                     if curr.right:
                         queue.appendleft(curr.right)
 
                     if curr.left:
                         queue.appendleft(curr.left)
-                else:
-                    curr = queue.popleft()
-                    print(f"curr = {curr.val}")
-                    level_nodes.append(curr.val)
+                
+                temp.append(curr.val)
 
-                    if curr.left:
-                        print(f"curr.left = {curr.left.val}")
-                        queue.append(curr.left)
-
-                    if curr.right:
-                        print(f"curr.right = {curr.right.val}")
-
-                        queue.append(curr.right)
-
-            print("\n")
-            output.append(level_nodes)
-
+            output.append(temp)
             toggle = not toggle
 
         return output
