518723aa5283984bdb51b5d64b442ec9feea9ae4
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-20 01:07:18 +0000

    Time: 55 ms (83.06%), Space: 21 MB (35.66%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,34 +1,32 @@
-from collections import defaultdict, deque
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         wordList = set(wordList)
         if endWord not in wordList:
             return 0
-
         mapping = defaultdict(list)
+        queue = deque([beginWord])
+        visited = {beginWord}
         L = len(beginWord)
-
         for word in wordList:
             for i in range(L):
                 mapping[word[:i] + "*" + word[i+1:]].append(word)
 
-        queue = deque([beginWord])
-        visited = {beginWord}
         steps = 1
-
         while queue:
-            for _ in range(len(queue)):
+
+            for i in range(len(queue)):
                 curr = queue.popleft()
+
                 if curr == endWord:
                     return steps
 
                 for i in range(L):
-                    pattern = curr[:i] + "*" + curr[i+1:]
-                    for neighbor in mapping[pattern]:
-                        if neighbor not in visited:
-                            visited.add(neighbor)
-                            queue.append(neighbor)
+                    for word in mapping[curr[:i] + "*" + curr[i+1:]]:
+                        if word not in visited:
+                            queue.append(word)
+                            visited.add(word)
+            
             steps += 1
 
         return 0
+
