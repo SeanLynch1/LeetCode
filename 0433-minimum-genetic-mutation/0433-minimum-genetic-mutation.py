c735470d0675105f93e47f6aa18cf49d2b4f4c8a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-20 00:12:55 +0000

    Time: 0 ms (100%), Space: 17.7 MB (65.59%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        
+        # need to build a graph
+        queue = deque([startGene])
+        visited = set()
+
+        def compare(x: str, y: str) -> int:
+            diff = 0
+
+            for i in range(len(x)):
+                if x[i] != y[i]:
+                    diff += 1
+
+            return diff
+
+        steps = 0
+        while queue:
+            for i in range(len(queue)):
+                curr = queue.popleft()
+
+                if curr == endGene:
+                    return steps
+
+                for gene in bank:
+                    if gene not in visited:
+                        diff = compare(curr, gene)
+
+                        if diff == 1:
+                            queue.append(gene)
+                            visited.add(gene)
+            steps += 1
+
+        return -1
+
+            
