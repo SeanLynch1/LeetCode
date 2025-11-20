a7013f42501f84ef0b0653a5e0e8ffed0c446e9a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-20 01:01:16 +0000

    Time: 87 ms (57.82%), Space: 25.4 MB (13.45%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,34 +1,47 @@
-from collections import defaultdict, deque
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordList = set(wordList)
-        if endWord not in wordList:
-            return 0
-
+        
         mapping = defaultdict(list)
-        L = len(beginWord)
-
-        for word in wordList:
-            for i in range(L):
-                mapping[word[:i] + "*" + word[i+1:]].append(word)
-
         queue = deque([beginWord])
         visited = {beginWord}
-        steps = 1
+        L = len(beginWord)
+        for word in wordList:
+            for i in range(L):
+                temp = list(word)
+                temp[i] = "*"
 
+                mapping["".join(temp)].append(word)
+
+        def compare(x: str, y: str) -> bool:
+
+            diff = 0
+            for i in range(L):
+                if x[i] != y[i]:
+                    diff += 1
+                    if diff > 1:
+                        return False
+            
+            return True
+
+        steps = 1
         while queue:
-            for _ in range(len(queue)):
+
+            for i in range(len(queue)):
                 curr = queue.popleft()
+
                 if curr == endWord:
                     return steps
 
                 for i in range(L):
-                    pattern = curr[:i] + "*" + curr[i+1:]
-                    for neighbor in mapping[pattern]:
-                        if neighbor not in visited:
-                            visited.add(neighbor)
-                            queue.append(neighbor)
+                    temp = list(curr)
+                    temp[i] = "*"
+
+                    for word in mapping["".join(temp)]:
+                        if word != curr and word not in visited and compare(curr, word):
+                            queue.append(word)
+                            visited.add(word)
+            
             steps += 1
 
         return 0
+
