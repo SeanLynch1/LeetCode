7246fce972e104d6982a4f4cea7bec419726df25
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-26 12:57:52 +0000

    Time: 0 ms (100%), Space: 17.9 MB (73.32%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,49 +1,38 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
-        # build a graph
-        graph = {}
-        output = []
+        mapping = defaultdict(dict)
+        res = []
 
         for i in range(len(equations)):
-
-            var_1 = equations[i][0]
-            var_2 = equations[i][1]
-
-            if var_1 not in graph:
-                graph[var_1] = {}
-
-            graph[var_1][var_2] = values[i]
-
-            if var_2 not in graph:
-                graph[var_2] = {}
             
-            graph[var_2][var_1] = 1 / values[i]
+            mapping[equations[i][0]][equations[i][1]] = values[i]
 
-        # create a helper function to search through graph
-        def helper(start: str, target: str, visited: set, product: float) -> float:
-            if start == target:
-                return product
+            mapping[equations[i][1]][equations[i][0]] = 1 / values[i]
 
-            if start not in visited:
-                visited.add(start)
+        print(mapping)
+        # traverse mapping
+        def helper(start, end, product):
+            if end in mapping[start]:
+                return product * mapping[start][end]
+            elif start in visited:
+                return -1
 
-            for divisor, value in graph[start].items():
-                if divisor in visited:
-                    continue
+            visited.add(start)
+            for key, val in mapping[start].items():
+                print(key, val, f"end = {end}")
+                output = helper(key, end, product * val)
 
-                res = helper(divisor, target, visited, product * value)
-
-                if res != -1:
-                    return res
+                if output != -1:
+                    return output
 
             return -1
 
-        for numerator, denomenator in queries:
-            if numerator not in graph or denomenator not in graph:
-                output.append(-1)
+        for s, e in queries:
+            visited = set()
+            if s not in mapping or e not in mapping:
+                res.append(-1)
             else:
-                visited = set()
-                output.append(helper(numerator, denomenator, visited, 1))
+                res.append(helper(s, e, 1))
 
-        return output
+        return res
