77d6b42669f628e364a1abf82f1fd9151c5f12a4
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-26 16:48:22 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,58 +1,52 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        graph = defaultdict(list)
-        output = []
+        mapping = defaultdict(set)
         visited = [0] * numCourses
-        found = set()
-        for course_1, course_2 in prerequisites:
-            graph[course_1].append(course_2)
-        
-        def helper(start: int) -> bool:
-            print(f"start = {start}")
-            if visited[start] == 2 or len(graph[start]) == 0:
-                return True
-            elif visited[start] == 1:
-                print(f"visited = {visited}")
-                print("FAILED")
+
+        pre = []
+        suff = []
+
+        for slot in prerequisites:
+            mapping[slot[0]].add(slot[1])
+
+            if slot[1] not in mapping:
+                mapping[slot[1]] = {}
+
+
+        def helper(start) -> bool:
+
+            if visited[start] == 1:
                 return False
+            if visited[start] == 2:
+                return True
 
             visited[start] = 1
-            res = True
 
-            for val in graph[start]:
-                res = helper(val)
-
-                if res == False:
-                    return res
-
-                
+            for val in mapping[start]:
+                if not helper(val):
+                    return False
+            
             visited[start] = 2
-            print(f"visited = {visited}")
 
-            return res
+            res.append(start)
+            return True
 
+        
+        for n in range(numCourses):
+            if n not in mapping:
+                pre.append(n)
+            else:
+                res = []
 
-        # loop through
-        for key, value in prerequisites:
-            
-            if value in found:
-                continue
+                print(f"n = {n}, visited = {visited}")
+                output = helper(n)
+                
+                if not output:
+                    return []
+                else:
+                    suff.extend(res)
 
-            print(f"testing with key = {key}, value = {value}")
-            res = helper(value)
-            
-            if res == False:
-                return []
-            
-            output.extend([value])
-            found.add(value)
-            print(f"output = {output}")
-            print(f"visited = {visited}")
-            print("\n")
+        pre.extend(suff)
 
-        for i in range(len(visited)):
-            if visited[i] == 0:
-                if i not in found:
-                    output.extend([i])
-        return output
+        return pre
