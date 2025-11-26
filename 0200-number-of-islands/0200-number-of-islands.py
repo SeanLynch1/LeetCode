b179ee45e72e5fad3ced14b208426559b3454339
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-26 11:54:33 +0000

    Time: 228 ms (92.22%), Space: 20.3 MB (42.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        islands = 0
-
+        
         no_rows = len(grid)
         no_cols = len(grid[0])
+        islands = 0
 
-        def helper(x: int, y: int):
-
-            if x < 0 or x == no_rows or y < 0 or y == no_cols or grid[x][y] == "0":
+        def helper(row, col):
+            if (row < 0 or row == no_rows) or (col < 0 or col == no_cols) or grid[row][col] == "0":
                 return
+            
+            grid[row][col] = "0"
 
-            # down
-            helper(x + 1, y)
-            grid[x][y] = "0"
+            # explore all directions
+            #down
+            helper(row + 1, col)
+
             # left
-            helper(x, y - 1)
+            helper(row, col - 1)
+
             # right
-            helper(x, y + 1)
+            helper(row, col + 1)
+
             # up
-            helper(x - 1, y)
+            helper(row - 1, col)
+
+            return
 
         for r in range(no_rows):
             for c in range(no_cols):
-                if grid[r][c] == "1":
-                    helper(r,c)
+                if grid[r][c] != "0":
+                    helper(r, c)
+
                     islands += 1
 
         return islands
