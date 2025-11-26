3fca9d27acb4ec291d033ec58090f68a76b18bc1
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-11-26 15:43:43 +0000

    Time: 15 ms (7.95%), Space: 20.7 MB (5.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,39 +1,44 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        graph = {}
-        visited = [0] * numCourses
-        # build graph
-        for course_1, course_2 in prerequisites:
-            if course_1 not in graph:
-                graph[course_1] = []
-            if course_2 not in graph:
-                graph[course_2] = []
+        mapping = defaultdict(set)
+        visited = defaultdict(int)
 
-            graph[course_1].append(course_2)
+        for slot in prerequisites:
+            mapping[slot[0]].add(slot[1])
+            mapping[slot[0]].add(slot[1]) 
 
-        # define helper funciton to loop through graph
+        print(mapping)
 
-        def helper(start: int) -> bool:
-            if len(graph[start]) == 0 or visited[start] == 2:
+        def helper(start) -> bool:
+            
+            if visited[start] == -1:
                 return True
-            elif visited[start] == 1:
+            elif visited[start] > 1:
                 return False
 
-            visited[start] = 1
-            for val in graph[start]:
-                outcome = helper(val)
-                if outcome == False:
-                    return outcome
-                    
-            visited[start] = 2
+            # already looped through contents
+            if len(mapping[start]) == 0:
+                visited[start] = -1
+                return True
+
+            visited[start] += 1
+            for val in mapping[start]:
+                output = helper(val)
+
+                if not output:
+                    visited[start] = 2
+                    return False
+
+            visited[start] = -1
+
             return True
 
-        for idx in prerequisites:
-            if visited[idx[1]] == 0:
-                res = helper(idx[1])
+        for n in range(0, numCourses):
+            if n in mapping:
+                res = helper(n)
 
                 if not res:
                     return False
-            
+
         return True
