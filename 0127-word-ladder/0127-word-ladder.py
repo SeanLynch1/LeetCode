c0eaa452ad3d1c32e9c0e021b443f8b6b44d017f
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-12-09 13:47:35 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,19 +2,16 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if not wordList:
             return 0
+        
+
+        wordList = set(wordList)
+
         if endWord not in wordList:
             return 0
 
-        wordList = set(wordList)
-        visited = set([beginWord])
         queue = deque([beginWord])
 
-        letters = set()
-
-        for word in wordList:
-            for w in word:
-                if w not in letters:
-                    letters.add(w)
+        letters = "abcdefghijklmnopqrstuv"
 
         moves = 1
         while queue:
@@ -31,9 +28,9 @@ class Solution:
                         if curr[i] != c:
                             find = curr[:i] + c + curr[i + 1:]
                             
-                            if find in wordList and find not in visited:
+                            if find in wordList:
                                 queue.append(find)
-                                visited.add(find)
+                                wordList.remove(find)
 
             moves += 1
 
