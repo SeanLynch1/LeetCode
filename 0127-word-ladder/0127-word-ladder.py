a17a26f7beb849928ddbac6a45c862c3793a1a06
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2025-12-09 13:48:25 +0000

    Time: 320 ms (36.05%), Space: 18.9 MB (64.18%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,32 +1,30 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if not wordList:
             return 0
-        
 
         wordList = set(wordList)
-
         if endWord not in wordList:
             return 0
 
         queue = deque([beginWord])
-
         letters = "abcdefghijklmnopqrstuvwxyz"
         moves = 1
+
         while queue:
             for _ in range(len(queue)):
-
                 curr = queue.popleft()
 
                 if curr == endWord:
                     return moves
-            
 
                 for i in range(len(curr)):
                     for c in letters:
                         if curr[i] != c:
                             find = curr[:i] + c + curr[i + 1:]
-                            
                             if find in wordList:
                                 queue.append(find)
                                 wordList.remove(find)
