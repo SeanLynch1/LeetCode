116a6f88581b27a3fc8dd1f878e5865efb239389
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-09 18:49:23 +0000

    Time: 31 ms (98.35%), Space: 20.7 MB (48.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,35 +1,41 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordSet = set(wordList)
-        if endWord not in wordSet:
+        if not wordList:
+            return 0
+        
+        wordList = set(wordList)
+
+        if endWord not in wordList:
             return 0
 
-        beginSet = {beginWord}
-        endSet = {endWord}
-        visited = set([beginWord, endWord])
         letters = "abcdefghijklmnopqrstuvwxyz"
+
+        begin_list = {beginWord}
+        end_list = {endWord}
+        visited = set([beginWord, endWord])
         moves = 1
 
-        while beginSet and endSet:
-            # Always expand the smaller frontier
-            if len(beginSet) > len(endSet):
-                beginSet, endSet = endSet, beginSet
+        while begin_list and end_list:
 
-            nextLevel = set()
-            for word in beginSet:
+            if len(begin_list) > len(end_list):
+                begin_list, end_list = end_list, begin_list
+            
+            next_list = set()
+            for word in begin_list:
+
                 for i in range(len(word)):
                     for c in letters:
                         if word[i] != c:
-                            newWord = word[:i] + c + word[i+1:]
-                            if newWord in endSet:
+                            find = word[:i] + c + word[i + 1:]
+                            
+                            if find in end_list:
                                 return moves + 1
-                            if newWord in wordSet and newWord not in visited:
-                                visited.add(newWord)
-                                nextLevel.add(newWord)
-            beginSet = nextLevel
+
+                            if find in wordList and find not in visited:
+                                next_list.add(find)
+                                visited.add(find)
+                
+            begin_list = next_list
             moves += 1
 
         return 0
