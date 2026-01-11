7c9d5d1430b35207d3d102b37540536b798af10c
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-11 14:15:09 +0000

    Time: 27 ms (98.96%), Space: 20.5 MB (53.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,41 +1,41 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if not wordList:
-            return 0
         
-        wordList = set(wordList)
+        word_list = set(wordList)
 
         if endWord not in wordList:
             return 0
 
-        letters = "abcdefghijklmnopqrstuvwxyz"
-
+        letters = 'abcdefghijklmnopqrstuvwxyz'
         begin_list = {beginWord}
         end_list = {endWord}
+
         visited = set([beginWord, endWord])
         moves = 1
 
-        while begin_list and end_list:
+        while begin_list:
 
             if len(begin_list) > len(end_list):
                 begin_list, end_list = end_list, begin_list
-            
+
+            # simulates a queue
             next_list = set()
+
             for word in begin_list:
 
                 for i in range(len(word)):
-                    for c in letters:
-                        if word[i] != c:
-                            find = word[:i] + c + word[i + 1:]
-                            
-                            if find in end_list:
-                                return moves + 1
+                    for l in letters:
 
-                            if find in wordList and find not in visited:
-                                next_list.add(find)
-                                visited.add(find)
-                
-            begin_list = next_list
+                        search_word = word[:i] + l + word[i + 1:]
+
+                        if search_word in end_list:
+                            return moves + 1
+
+                        if search_word in word_list and search_word not in visited:
+                            next_list.add(search_word)
+                            visited.add(search_word)
+
             moves += 1
+            begin_list = next_list
 
         return 0
