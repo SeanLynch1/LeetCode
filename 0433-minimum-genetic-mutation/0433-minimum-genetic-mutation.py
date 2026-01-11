165cbc56f7b4579a77ad36b2afd30839dafd41f4
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-11 20:15:57 +0000

    Time: 0 ms (100%), Space: 19.4 MB (6.29%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -10,6 +10,8 @@ class Solution:
 
         begin_list = {startGene}
         end_list = {endGene}
+        visited = set()
+
         moves = 0
 
         while begin_list:
@@ -17,24 +19,23 @@ class Solution:
             if len(begin_list) > len(end_list):
                 begin_list, end_list = end_list, begin_list
 
-            for curr_word in begin_list:
+            next_list = set()
 
-                next_list = set()
-
-                for j in range(len(curr_word)):
+            for word in begin_list:
+                for i in range(len(word)):
                     for l in letters:
 
-                        search_word = curr_word[:j] + l + curr_word[ j+1:]
+                        search_word = word[:i] + l + word[i + 1:]
 
                         if search_word in end_list:
                             return moves + 1
 
-                        if search_word in bank:
+                        if search_word in bank and search_word not in visited:
                             next_list.add(search_word)
-                            bank.remove(search_word)
+                            visited.add(search_word)
             
-            begin_list = next_list
             moves += 1
+            begin_list = next_list
 
         return -1
 
