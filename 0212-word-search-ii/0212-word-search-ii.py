07b317debf279f71a820dd1c7c3d67656aeb14a0
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-11 13:13:56 +0000

    Time: 3953 ms (67.96%), Space: 21 MB (8.42%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -6,62 +6,57 @@ class TrieNode:
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         
-        output = []
         trie = TrieNode()
+        output = []
         rows = len(board)
         cols = len(board[0])
-
-        # build trie
+        # create trie
         for word in words:
             curr = trie
-
-            for i in range(len(word)):
-                
-                l = word[i]
+            for l in word:
 
                 if l not in curr.children:
                     curr.children[l] = TrieNode()
-                
-                curr = curr.children[l]
 
+                curr = curr.children[l]
+            
             curr.word = word
 
-        def dfs(curr: TrieNode, x: int, y: int):
+        
+        # dfs the board, referencing the trie
+        def dfs(curr: TrieNode, x : int, y : int):
 
             l = board[x][y]
+
             if l not in curr.children:
                 return
             
-            if curr.children[l].word is not None:
+            if curr.children[l].word != None:
                 output.append(curr.children[l].word)
                 curr.children[l].word = None
-
-            # mark visited, if we snake back to the tile later this is a no no
+            
+            # prevent looping with snaking back to same tile
             board[x][y] = "#"
 
-            # check up
+            # up
             if x - 1 >= 0:
                 dfs(curr.children[l], x - 1, y)
-
-            # check left
+            # left
             if y - 1 >= 0:
-                dfs(curr.children[l], x, y - 1)
-
-            # check right
+                dfs(curr.children[l],x, y - 1)
+            # right
             if y + 1 < cols:
-                dfs(curr.children[l], x, y + 1)
-
-            # check down
+                dfs(curr.children[l],x, y + 1)
+            # down
             if x + 1 < rows:
-                dfs(curr.children[l], x + 1, y)
+                dfs(curr.children[l],x + 1, y)
 
             board[x][y] = l
 
-            return
+            return 
 
         for r in range(rows):
             for c in range(cols):
-                dfs(trie, r, c)   
-            
-        return output
+                dfs(trie, r, c)
                 
+        return output
