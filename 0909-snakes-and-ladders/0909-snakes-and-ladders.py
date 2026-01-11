915da60161ead04fdcad2df65942864099c50116
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-11 21:07:50 +0000

    Time: 19 ms (78.14%), Space: 19.3 MB (5.69%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -8,20 +8,20 @@ class Solution:
         tiles = n * n
         squares = tiles
         offset = n % 2 == 0
-        
+
+        def left_to_right(x,y):
+            if board[x][y] != - 1:
+                mapping[tiles] = board[x][y]
+            else:
+                mapping[tiles] = -1
+
         for i in range(n):
             for j in range(n):
                 
                 if offset:
-                    if board[i][j] != - 1:
-                        mapping[tiles] = board[i][j]
-                    else:
-                        mapping[tiles] = -1
+                   left_to_right(i,j)
                 else:
-                    if board[i][n - j - 1] != -1:
-                        mapping[tiles] = board[i][n - j - 1]
-                    else:
-                        mapping[tiles] = -1
+                    left_to_right(i,n - j - 1)
 
                 tiles -= 1
             
