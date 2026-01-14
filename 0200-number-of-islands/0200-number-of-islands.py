1ddf9de55f515b40f6155f455bbfea235b70d6bb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-14 12:30:11 +0000

    Time: 262 ms (32.59%), Space: 21.6 MB (39.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        no_rows = len(grid)
-        no_cols = len(grid[0])
         islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
 
-        def helper(row, col):
-            if (row < 0 or row == no_rows) or (col < 0 or col == no_cols) or grid[row][col] == "0":
+        directions = [(-1,0), (1, 0), (0,-1), (0,1)]
+        def dfs(x , y) -> None:
+
+            if grid[x][y] != "1":
                 return
-            
-            grid[row][col] = "0"
 
-            # explore all directions
-            #down
-            helper(row + 1, col)
+            grid[x][y] = "#"
 
-            # left
-            helper(row, col - 1)
-
-            # right
-            helper(row, col + 1)
-
-            # up
-            helper(row - 1, col)
+            for nx, ny in directions:
+                if x + nx >= 0 and x + nx < rows and y + ny >= 0 and y + ny < cols:
+                    dfs(x + nx, y + ny)
 
             return
 
-        for r in range(no_rows):
-            for c in range(no_cols):
-                if grid[r][c] != "0":
-                    helper(r, c)
-
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r,c)
                     islands += 1
 
         return islands
