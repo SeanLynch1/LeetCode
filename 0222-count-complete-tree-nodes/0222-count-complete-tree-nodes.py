2a75e952e5c418a3ab35b951b0678dace8f33433
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-15 12:32:41 +0000

    Time: 4 ms (39.51%), Space: 23.9 MB (6.7%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -1,30 +1,5 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def getDepth(self, node: TreeNode) -> int:
-        depth = 0
-        while node:
-            depth += 1
-            node = node.left
-
-        return depth
-
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        
         if not root:
             return 0
-
-        left_depth = self.getDepth(root.left)
-        right_depth = self.getDepth(root.right)
-
-        # left side is perfect (possibly)
-        if left_depth == right_depth:
-            return self.countNodes(root.right) + (2 ** left_depth)
-            
-        # right side is perfect (possibly)
-        else:
-            return self.countNodes(root.left) + (2 ** right_depth)
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
