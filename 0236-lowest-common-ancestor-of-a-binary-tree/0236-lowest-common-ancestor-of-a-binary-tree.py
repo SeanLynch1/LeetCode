460e798e7ba4c47e1eb8560ad67cc4829a259cb6
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-15 11:07:39 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,30 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
+# Definition for a binary tree root.
+# class Treeroot:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(self, root: 'Treeroot', p: 'Treeroot', q: 'Treeroot') -> 'Treeroot':
         
-        def dfs(node: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-            
-            if not node or node == p or node == q:
-                return node
+        if not root or root == p or root == q:
+            return root
 
-            # check left
-            left = dfs(node.left, p, q)
-            
-            # check right
-            right = dfs(node.right, p, q)
+        # check left
+        left = lowestCommonAncestor(root.left, p, q)
+        
+        # check right
+        right = lowestCommonAncestor(root.right, p, q)
 
-            if left and right:
-                return node
+        if left and right:
+            return root
 
-            return left or right
-
-        return dfs(root,p,q)
+        return left or right
 
 
 
