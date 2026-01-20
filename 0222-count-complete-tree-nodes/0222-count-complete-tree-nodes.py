300f40e62a8d48ba6c0dd9487f0afa426a2372b2
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-20 22:19:47 +0000

    Time: 0 ms (100%), Space: 24 MB (6.04%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,9 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    def getDepth(self, root: Optional[TreeNode]) -> int:
+        depth = 0
+
+        while root:
+            root = root.left
+            depth += 1
+
+        return depth
+
+
     def countNodes(self, root: Optional[TreeNode]) -> int:
-       if not root:
-        return 0
+        if not root:
+            return 0
 
-       return self.countNodes(root.left) + self.countNodes(root.right) + 1
+        left_depth = self.getDepth(root.left)
+        right_depth = self.getDepth(root.right)
+        
+        if left_depth == right_depth:
+            return self.countNodes(root.right) + 2 ** left_depth
+        else:
+            return self.countNodes(root.left) + 2 ** right_depth
 
+        
+        
