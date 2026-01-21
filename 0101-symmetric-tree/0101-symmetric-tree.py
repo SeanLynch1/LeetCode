7a95c6734513d1258d12d7c647815165f25a0ede
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-21 14:24:10 +0000

    Time: 0 ms (100%), Space: 19.5 MB (15.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -17,23 +17,21 @@ class Solution:
 
         while queue:
 
-            for i in range(len(queue)):
+            curr_left, curr_right = queue.popleft()
 
-                curr_left, curr_right = queue.popleft()
+            if (not curr_left.left and curr_right.right) or (not curr_right.right and curr_left.left):
+                return False
+            
+            if (not curr_left.right and curr_right.left) or (curr_left.right and not curr_right.left):
+                return False
 
-                if (not curr_left.left and curr_right.right) or (not curr_right.right and curr_left.left):
-                    return False
-                
-                if (not curr_left.right and curr_right.left) or (curr_left.right and not curr_right.left):
-                    return False
+            if curr_left.val != curr_right.val:
+                return False
 
-                if curr_left.val != curr_right.val:
-                    return False
+            if curr_left.left and curr_right.right:
+                queue.append((curr_left.left, curr_right.right))
 
-                if curr_left.left and curr_right.right:
-                    queue.append((curr_left.left, curr_right.right))
-
-                if curr_right.left and curr_left.right:
-                    queue.append((curr_left.right, curr_right.left))
+            if curr_right.left and curr_left.right:
+                queue.append((curr_left.right, curr_right.left))
 
         return True
