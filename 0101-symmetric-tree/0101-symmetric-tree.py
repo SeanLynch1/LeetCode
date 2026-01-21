08de7ab12e46ff5ab15129dd80bbb01c60846189
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-21 14:41:03 +0000

    Time: 0 ms (100%), Space: 19.5 MB (15.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -7,31 +7,23 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
-        if not root.left and root.right or root.left and not root.right:
-            return False
-
-        if not root.left and not root.right:
-            return True
-
         queue = deque([(root.left, root.right)])
 
         while queue:
 
             curr_left, curr_right = queue.popleft()
 
-            if (not curr_left.left and curr_right.right) or (not curr_right.right and curr_left.left):
+            if not curr_left and not curr_right:
+                continue
+
+            if not curr_left or not curr_right:
                 return False
             
-            if (not curr_left.right and curr_right.left) or (curr_left.right and not curr_right.left):
-                return False
-
             if curr_left.val != curr_right.val:
                 return False
 
-            if curr_left.left and curr_right.right:
-                queue.append((curr_left.left, curr_right.right))
+            queue.append((curr_left.left, curr_right.right))
 
-            if curr_right.left and curr_left.right:
-                queue.append((curr_left.right, curr_right.left))
+            queue.append((curr_left.right, curr_right.left))
 
         return True
