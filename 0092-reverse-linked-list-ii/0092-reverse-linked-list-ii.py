6a81992dbf03ad98b1b3c514ec5f6eb3cb7fb032
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-24 15:27:14 +0000

    Time: 0 ms (100%), Space: 19.5 MB (10.01%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,33 +5,45 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        # since head could be what gets reversed, if we return it, we are returning the listnode that head originally pointed to, so instead make a new listNode that points to the first listNode, and return it's next, we will update what it points to but we can confirm it will always be first, head may not always be first. we are not making a copy of the current linked list, we are modifying it in place, but just adding a pointer to the first node, as the first node could change depending on where the reverse begins
         
-        dummy = ListNode(0)
-        dummy.next = head
+        if left == right:
+            return head
 
-        # set prev
-        prev = dummy
 
-        # iterate through the linked list until we reach the start of the reversal
+        dummy_node = ListNode(0, head)
 
-        for i in range(left - 1):
-            prev = prev.next
+        curr = head
+        prev = dummy_node
 
-        start = prev.next
-        then = start.next
+        idx = 1
+        while curr:
+            print(curr.val)
+            if idx == left:
+                print("hi")
+                start = prev
+                fast = curr.next
+                print(f"right - left = {right - left}")
+                for _ in range(right - left + 1):
+                    
+                    print("hello")
+                    curr.next = prev
+                    prev = curr
 
-        for i in range(right - left):
-            # start will always equal start, just update who it points to, we want it to point to the .next of right
-            start.next = then.next
-            # then, who will move forward one each iteration, always will point to its previous, the reason it points to prev.next it because then moves forward one, and prev.next points to what then used to be
-            then.next = prev.next
+                    curr = fast
+                    if fast:
+                        fast = fast.next
 
-            # this step will always be wrong until the last iteration of the loop
-            prev.next = then
+                start.next.next = curr
+                start.next = prev
+                break
 
-            # move then forward to what it originally pointed to
-            then = start.next
-            
+            prev = curr
+            curr = curr.next
+            idx += 1
 
-        return dummy.next
+        return dummy_node.next
+
+
+
+
+
