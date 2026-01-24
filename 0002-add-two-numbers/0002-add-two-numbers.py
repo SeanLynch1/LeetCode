f3b1f2048f9b8a4942b430e90f632625476bbc54
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-24 22:38:27 +0000

    Time: 15 ms (9.17%), Space: 19.5 MB (14.41%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,38 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
+
+    def get_num(self, node: ListNode) -> int:
+        first_num = 0
+        dummy_node = ListNode(0, node)
+        prev = dummy_node
+
+        while node:
+            next_node = node.next
+            node.next = prev
+            prev = node
+            node = next_node
+
+        dummy_node.next.next = None
+        curr = prev
+        while curr:
+            first_num = (first_num * 10) + curr.val
+            curr = curr.next
+
+        return first_num
+
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        string_left = []
-        string_right = []
-
-        curr = l1
-        while curr:
-            string_left.append(str(curr.val))
+        total = str(self.get_num(l1) + self.get_num(l2))
+        curr = None
+        dummy_node = ListNode(0)
+        prev = dummy_node
+        for i in range(len(total)-1, -1, -1):
+            print(total[i])
+            curr = ListNode(int(total[i]))
+            prev.next = curr
+            prev = curr
             curr = curr.next
 
-        string_left = string_left[-1::-1]
-        string_left = int(''.join(string_left))
+            
 
-        curr = l2
-        while curr:
-            string_right.append(str(curr.val))
-            curr = curr.next
-
-        string_right = string_right[-1::-1]
-        string_right = int(''.join(string_right))
-
-        total = str(string_left + string_right)
-
-        head = ListNode(int(total[-1]))
-        curr = head
-
-        for i in range(len(total) - 2, -1, -1):
-            curr.next = ListNode(int(total[i]))
-            curr = curr.next
-
-        return head
-
-
+        return dummy_node.next
+        
