6d424267c666dbd5df88d4137b548a46789fbc8a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-25 12:43:01 +0000

    Time: 9 ms (44.8%), Space: 24.2 MB (5.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,27 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        mapping = set("abcdefghijklmnopqrstuvwxyz0123456789")
-
-        left = 0
-        right = len(s) - 1
- 
-        while left < right:
-            left_char = s[left].lower()
-            right_char = s[right].lower()
-
-            if left_char not in mapping:
-                left += 1
-                continue
-
-            if right_char not in mapping:
-                right -= 1
-                continue
-
-            if left_char != right_char:
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
+        filtered = [c.lower() for c in s if c.isalnum()]
+        return filtered == filtered[::-1]
