ec3ca2165aa352a0a05d95728d13382ccc6fb9a2
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-25 13:15:09 +0000

    Time: 0 ms (100%), Space: 19.4 MB (23%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -8,17 +8,10 @@ class Solution:
             if left >= len(s):
                 return True
 
-            print(f"left = {s[left]}, right = {t[right]}")
-            print("")
-
             while right < len(t) and s[left] != t[right]:
                 right += 1
 
             left += 1
             right += 1
 
-        if left >= len(s) and right <= len(t):
-            return True
-
-        print(f"left = {left}, right = {right}")
-        return False
+        return left >= len(s) and right <= len(t)
