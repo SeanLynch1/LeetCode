2228530c3fc4752ff3fff98f62b9ea2d6884c80a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-25 20:25:31 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,39 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
         
-        i = 0
+        output = []
+        nums.sort()
 
-        while i < len(nums) and nums[i] <= 0:
+        for i in range(len(nums)):
 
-            if i > 0 and nums[i] == nums[i-1]:
-                print(f"i = i -1, setting i to {i+1}")
-                i+= 1
-                continue
+            target = nums[i] 
+            left = i + 1
+            right = len(nums) - 1
             
-            # look for combos
-            j = i + 1
-            k = len(nums) - 1
-            target = 0 - nums[i]
+            if i > 0 and target == nums[i - 1]:
+                continue
 
-            while j < k:
-                total = nums[j] + nums[k]
-                if total == target:
-                    
-                    if res: 
-                        if (nums[j] == res[-1][1] and nums[k] == res[-1][2]):
-                            k -= 1
-                            continue
-                        
-                    res.append([nums[i],nums[j], nums[k]])
-                    
-                    k -= 1
-                elif total > target:
-                    k -= 1
+            while left < right:
+                
+                curr_left = nums[left]
+                curr_right = nums[right]
+                two_sum = curr_right + curr_left
+
+                if target + two_sum == 0:
+                    output.append([target, curr_left, curr_right])
+                
+
+                if target + two_sum >= 0:
+                    right -= 1
                 else:
-                    j += 1
+                    left += 1
 
-            i += 1
+                
 
-        return res
+        return output
