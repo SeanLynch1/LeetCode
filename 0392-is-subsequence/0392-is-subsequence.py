77509af5b909bef87186c83c721e3f0763b95dc9
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-25 13:16:09 +0000

    Time: 0 ms (100%), Space: 19.3 MB (23%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -3,10 +3,8 @@ class Solution:
 
         left = 0
         right = 0
-
-        while right < len(t):
-            if left >= len(s):
-                return True
+        while right < len(t) and left < len(s):
+            
 
             while right < len(t) and s[left] != t[right]:
                 right += 1
