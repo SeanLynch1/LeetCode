c60901965ee37181f10c779b37c0ba63351abe89
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-25 20:30:13 +0000

    Time: 748 ms (25.82%), Space: 22.4 MB (13.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -22,13 +22,16 @@ class Solution:
                 if target + two_sum == 0:
                     output.append([target, curr_left, curr_right])
 
-                    while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
                         left += 1
 
-                    while right > left and nums[right] == nums[right - 1]:
+                    while right > left and nums[right] == nums[right + 1]:
                         right -= 1
                 
-                if target + two_sum < 0:
+                elif target + two_sum < 0:
                     left += 1
                 else:
                     right -= 1
