f117cefd72ea20b0dbea5c5507e4126eeb17ec12
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-25 13:14:12 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,16 +1,21 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-
-        if len(s) == 0:
+        if not s:
             return True
+            
+        left = 0
+        right = 0
 
-        found = 0
-        for l in t:
-            print(l)
-            if l == s[found]:
-                print("found : ", l)
-                found += 1
-            if found == len(s):
+        while right < len(t):
+            
+            while right < len(t) and s[left] != t[right]:
+                right += 1
+
+            left += 1
+            right += 1
+
+            print(f"left = {left}, right = {right}")
+            if left >= len(s):
                 return True
-        
+
         return False
