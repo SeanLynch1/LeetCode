f7cf934ec9f975f826e6d7039f64d0ad3572f365
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-26 17:55:01 +0000

    Time: 25 ms (66.6%), Space: 20.1 MB (6.68%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -7,7 +7,6 @@ class Solution:
 
         for w in words:
             words_dict[w] += 1
-        words = words_dict
 
         output = []
 
@@ -20,46 +19,34 @@ class Solution:
             for right in range(left, len(s), word_length):
                 
                 curr_joined = s[right: right + word_length]
-                print(f"curr_joined = {curr_joined}")
 
-                if curr_joined in words:   
-                    if seen[curr_joined] < words[curr_joined]:
+                if curr_joined in words_dict:   
+                    if seen[curr_joined] < words_dict[curr_joined]:
                         
                         seen[curr_joined] += 1
                         found += 1
 
-                        print(f"curr_joined = {curr_joined}, found = {found}")
                         if found == word_count:
-                            print(f"{curr_joined} found!, adding {left} to output")
                             output.append(left)
                             left_word = s[left: left + word_length]
                             seen[left_word] -= 1
                             found -= 1
                             left += word_length
-                            print(f"seen = {seen}, found = {found} \n")
                     else:
-                        print(f"{curr_joined}, already in seen :/")
                         seen[curr_joined] += 1
                         found += 1
 
-                        while seen[curr_joined] > words[curr_joined]:
+                        while seen[curr_joined] > words_dict[curr_joined]:
                             left_word = s[left: left + word_length]
-                            print(f"left_word = {left_word}")
                             seen[left_word] -= 1
 
                             left += word_length
                             found -= 1
 
-                        print(f"left now = {left}")
-                        print(f"seen = {seen}, found = {found} \n")
                 else:
                     seen = defaultdict(int)
                     left = right + word_length
                     found = 0
-
-                    print(f"{curr_joined}, not in seen :(")
-                    print(f"seen = {seen}, found = {found} \n")
-
 
         
         return output
