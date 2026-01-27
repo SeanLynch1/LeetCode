80779244b8737d50d949aa579b70bf9d0fe304db
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-27 12:30:15 +0000

    Time: 31 ms (34.46%), Space: 26.3 MB (23.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,8 +3,7 @@ class Solution:
         
         prefixes = [1]
         for i in range(1, len(nums)):
-            prev_nums = nums[i - 1]
-            prefixes.append(prev_nums * prefixes[-1])
+            prefixes.append(nums[i - 1] * prefixes[-1])
 
         for j in range(len(prefixes)-1,0,-1):
             prefixes[j - 1] *= nums[j]
