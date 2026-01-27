fa33171a16bf83ee8e44dca1c2c06e320d7d9a21
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-27 12:26:18 +0000

    Time: 26 ms (48.28%), Space: 25.5 MB (41.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,13 +6,9 @@ class Solution:
             prev_nums = nums[i - 1]
             prefixes.append(prev_nums * prefixes[-1])
 
-        print(prefixes)
         last = 1
         for j in range(len(prefixes)-1,-1,-1):
             prefixes[j] *= last
             last *= nums[j]
-
-
-        print(prefixes)
 
         return prefixes
