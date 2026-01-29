b7d2bfa0f8e7e060c10b169da3cb317227b1c0eb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-01-29 09:36:13 +0000

    Time: 0 ms (100%), Space: 19.4 MB (31.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,34 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        n = len(nums1)
+        for i in range(1, len(nums2) + 1):
+            nums1[n-i] = nums2[i-1]
 
-        nums1[m:] = nums2
-        print(nums1)
-
-        nums1.sort()
-        print(nums1)
-
-        # define three variables
-        i = m - 1 # last digit in nums1
-        j = n - 1 # last digit in nums2
-        k = m + n - 1 # last position in nums1
-
-        # idea:
-        # compare last digits in both lists and move backwards accordingly, a total of m + n comparisons should be made
-
-        # loop backwards
-        '''while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-
-            k -= 1
-        
-        # if i was fully iterated through but j was not that means all remaining elements in j have to be less than all elements in i
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1'''
+        print(nums1.sort())
