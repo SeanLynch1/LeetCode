e95acaea472352ff3505477dc1bc50c61b2c5138
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-03 12:26:25 +0000

    Time: 60 ms (6.79%), Space: 21.2 MB (43.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return(nums[len(nums) // 2])
+        
+        temp = 1
+        n = nums[0]
+        max_sequence = 0
 
+        nums.sort()
+
+        print(nums)
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                temp += 1
+
+                if temp >= max_sequence:
+                    print(f"temp = {temp}, max_sequence = {max_sequence}, n = {nums[i]}")
+                    n = nums[i]
+                    max_sequence = temp
+
+            else:
+                temp = 1
+
+            
+
+        return n
