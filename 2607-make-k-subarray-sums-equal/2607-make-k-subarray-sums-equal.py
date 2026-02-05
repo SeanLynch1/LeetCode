0fbaaee633be165c33fb3251b22e94bcea75e4fb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-05 15:10:13 +0000

    Time: 199 ms (15.58%), Space: 36.2 MB (22.08%) - LeetHub

---
+++ 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.py
@@ -0,0 +1,44 @@
+class Solution:
+    def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
+        
+        n = len(arr)
+        medians = []
+        output = 0
+
+        def gcd(a:int, b:int):
+            
+            if b == 0:
+                return a
+            
+            return gcd(b, a%b)
+
+        cycles = gcd(len(arr), k)
+
+        for c in range(cycles):
+            original = c
+            jump = (c + k) % n
+            temp = [arr[c % n]]
+
+            while jump != c:
+                temp.append(arr[jump])
+
+                jump += k
+                jump %= n
+
+            temp.sort()
+            median_temp = temp[int(len(temp) / 2)]
+            medians.append(median_temp)
+        
+        for c in range(cycles):
+            original = c
+            jump = (c + k) % n
+            output += abs(medians[c] - arr[c % n])
+
+            while jump != c:
+                output += abs(medians[c] - arr[jump])
+
+                jump += k
+                jump %= n
+
+            
+        return output
