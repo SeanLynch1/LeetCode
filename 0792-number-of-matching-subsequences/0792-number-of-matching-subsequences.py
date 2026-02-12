615c1080d5f204d4e147ec519d64513fed4deca6
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-12 14:42:01 +0000

    Time: 249 ms (66.99%), Space: 23.6 MB (6.6%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -1,32 +1,30 @@
+from bisect import bisect_right
+from collections import defaultdict, Counter
+from typing import List
+
 class Solution:
-
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        
-        # build a dict
-        available = defaultdict(list)
+        pos = defaultdict(list)
+        for i, ch in enumerate(s):
+            pos[ch].append(i)
+
         total = 0
+        freq = Counter(words)
 
-        for i in range(len(s)):
-            available[s[i]].append(i)
-
-        for word in words:
-            last_idx = -1
-
-            for i in range(len(word)):
-                
-                letter = word[i]
-
-                if letter in available:
-                    arr = available[letter]
-
-                    target = bisect_right(arr, last_idx)
-
-                    if target == len(arr):
-                        break
-                    last_idx = arr[target]
-                    if i == len(word) -1:
-                        total += 1
-                else:
+        for w, count in freq.items():
+            last = -1
+            ok = True
+            for ch in w:
+                arr = pos.get(ch)
+                if not arr:
+                    ok = False
                     break
+                j = bisect_right(arr, last)
+                if j == len(arr):
+                    ok = False
+                    break
+                last = arr[j]
+            if ok:
+                total += count
 
         return total
