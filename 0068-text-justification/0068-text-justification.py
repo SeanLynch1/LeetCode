f777dbd824aba3bd9ef7538a23884fb05e7f8ca0
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-12 10:28:49 +0000

    Time: 0 ms (100%), Space: 19.4 MB (56.71%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -20,14 +20,11 @@ class Solution:
                     spaces_per_gap = spaces_needed // gap_slots
                     remaining_spaces = spaces_needed % gap_slots
 
-                    print(f"temp = {temp}")
                     for i in range(gap_slots):
                         temp[i] += " " * spaces_per_gap
                         if remaining_spaces > 0:
                             temp[i] += " "
                             remaining_spaces -= 1
-
-                        print(f"t = {temp[i]}")
                 else:
                     temp[-1] += " " * spaces_needed
                     
@@ -36,29 +33,18 @@ class Solution:
                 temp = [word]
                 line_length = len(word)
                 gap_slots = 1
-                print("")
 
         # handling last line
         spaces_needed = maxWidth - line_length
 
-        print(f"temp = {temp}")
         for i in range(len(temp)):
-            print(f"t = {temp[i]}")
-            
             if i == len(temp) - 1:
-                print(f"spaces_needed = {spaces_needed}")
                 temp[i] += " " * spaces_needed
             else:
                 temp[i] += " "
                 spaces_needed -= 1
 
-            
-            
         line = "".join(temp)
         output.append(line)
 
-        for line in output:
-            print(line)
-
-        
         return output
