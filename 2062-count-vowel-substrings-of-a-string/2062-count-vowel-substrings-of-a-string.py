114f434d8dd2a4a292e24dc1c70bc68c43f889e5
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-16 16:45:40 +0000

    Time: 15 ms (47.12%), Space: 19.4 MB (47.53%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,37 +1,18 @@
-from collections import defaultdict
-
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
+        
         vowels = set("aeiou")
+        total = 0
 
-        def atMost(k: int) -> int:
-            cnt = defaultdict(int)
-            left = 0
-            distinct = 0
-            ans = 0
+        for i in range(len(word)):
 
-            for right, ch in enumerate(word):
-                if ch not in vowels:
-                    # reset on consonant
-                    cnt.clear()
-                    left = right + 1
-                    distinct = 0
-                    continue
+            right = i
+            found = set()
+            while right < len(word) and word[right] in vowels:
+                found.add(word[right])
+                if len(found) == 5:
+                    total += 1
 
-                if cnt[ch] == 0:
-                    distinct += 1
-                cnt[ch] += 1
+                right += 1
 
-                while distinct > k:
-                    c = word[left]
-                    cnt[c] -= 1
-                    if cnt[c] == 0:
-                        distinct -= 1
-                    left += 1
-
-                # number of valid substrings ending at right
-                ans += right - left + 1
-
-            return ans
-
-        return atMost(5) - atMost(4)
+        return total
