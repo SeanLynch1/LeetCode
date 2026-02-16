986eab2cbebb57c98a433f81c1c7b5c339394c42
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-16 16:37:15 +0000

    Time: 51 ms (16.71%), Space: 19.5 MB (18.22%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,24 +1,72 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        
-
         total = 0
-
         vowels = set("aeiou")
+        found = defaultdict(int)
 
+        left = 0
+        right = 0
 
-        for left in range(len(word)):
+        while left < len(word) - 3:
             
-            right = left
-            found = set()
+            if left > right:
+                right = left
+            print(f"left = {left} - {word[left]}")
+            print(f"right = {right} - {word[right]}")
+            print(f"left to right = {word[left:right + 1]}")
+            print(f"found = {found}")
+            
 
-            while right < len(word) and word[right] in vowels:
-                found.add(word[right])
+            if word[left] in vowels:
+                while right < len(word) and word[right] in vowels:
+                    found[word[right]] += 1
 
-                if len(found) == 5:
-                    total += 1
+                    if len(found) == 5:
+                        print(f"found in forward check: {word[left:right + 1]}")
+                        total += 1
+                    
+                    right += 1
+
+                right -= 1
+
+                found[word[left]] -= 1
+                if found[word[left]] <= 0:
+                    del found[word[left]]
+
+                left += 1
                 
-                right += 1
+                if left > right:
+                    continue
+
+                print(f"left = {left} - {word[left]}")
+                print(f"right = {right} - {word[right]}")
+                print(f"left to right = {word[left:right + 1]}")
+                print(f"found = {found}")
+              
+                while right > left and len(found) == 5:
+                    found[word[right]] -= 1
+                    
+                    total += 1
+                    print(f"found in back check : {word[left:right + 1]}")
+
+                    if found[word[right]] <= 0:
+                        del found[word[right]]
+                    else:
+                        right -= 1
+                
+                found[word[left]] -= 1
+                if found[word[left]] <= 0:
+                    del found[word[left]]
+
+                left += 1
+
+                
+            else:
+                found = defaultdict(int)
+                left += 1
+                right = left
 
 
+            print("\n")
+            
         return total
