5e02eb9b11f00e902cae66fab583c8439b390e78
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-16 16:44:24 +0000

    Time: 1 ms (77.53%), Space: 19.4 MB (47.53%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,54 +1,37 @@
+from collections import defaultdict
+
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        total = 0
         vowels = set("aeiou")
-        found = defaultdict(int)
 
-        left = 0
-        right = 0
+        def atMost(k: int) -> int:
+            cnt = defaultdict(int)
+            left = 0
+            distinct = 0
+            ans = 0
 
-        while left < len(word) - 3:
-            
-            if word[left] in vowels:
-                while right < len(word) and word[right] in vowels:
-                    found[word[right]] += 1
-
-                    if len(found) == 5:
-                        total += 1
-                    
-                    right += 1
-
-                right -= 1
-
-                found[word[left]] -= 1
-                if found[word[left]] <= 0:
-                    del found[word[left]]
-
-                left += 1
-                if left > right:
-                    right = left
+            for right, ch in enumerate(word):
+                if ch not in vowels:
+                    # reset on consonant
+                    cnt.clear()
+                    left = right + 1
+                    distinct = 0
                     continue
 
-                while right > left and len(found) == 5:
-                    found[word[right]] -= 1
-                    
-                    total += 1
+                if cnt[ch] == 0:
+                    distinct += 1
+                cnt[ch] += 1
 
-                    if found[word[right]] <= 0:
-                        del found[word[right]]
-                    else:
-                        right -= 1
-                
-                found[word[left]] -= 1
-                if found[word[left]] <= 0:
-                    del found[word[left]]
+                while distinct > k:
+                    c = word[left]
+                    cnt[c] -= 1
+                    if cnt[c] == 0:
+                        distinct -= 1
+                    left += 1
 
-                left += 1
+                # number of valid substrings ending at right
+                ans += right - left + 1
 
-                
-            else:
-                found = defaultdict(int)
-                left += 1
-                right = left
+            return ans
 
-        return total
+        return atMost(5) - atMost(4)
