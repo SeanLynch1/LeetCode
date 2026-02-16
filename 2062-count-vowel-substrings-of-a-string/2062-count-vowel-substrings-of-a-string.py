6fe7ab1da1aa01c73b402b73a1c5c08b30cc278f
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-16 16:45:52 +0000

    Time: 19 ms (29.45%), Space: 19.4 MB (47.53%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,18 +1,16 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        
-        vowels = set("aeiou")
+        idx = {'a':0,'e':1,'i':2,'o':3,'u':4}
         total = 0
+        n = len(word)
 
-        for i in range(len(word)):
-
+        for i in range(n):
+            mask = 0
             right = i
-            found = set()
-            while right < len(word) and word[right] in vowels:
-                found.add(word[right])
-                if len(found) == 5:
+            while right < n and word[right] in idx:
+                mask |= 1 << idx[word[right]]
+                if mask == 0b11111:
                     total += 1
-
                 right += 1
 
         return total
