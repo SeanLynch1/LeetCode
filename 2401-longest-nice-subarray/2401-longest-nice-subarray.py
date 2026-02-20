862616466ed8fde0d0f33948f2e5a608b88b2f35
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 14:19:44 +0000

    Time: 59 ms (83.33%), Space: 33.6 MB (16.05%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -11,22 +11,15 @@ class Solution:
             bit_start = nums[i]
 
             right = i + 1
-            print(f"start word = {bit_start}")
 
             while right < len(nums) and nums[right] & bit_start == 0:
                 
-                print(f"word = {nums[right]}")
-                print(f"binary of start word {bin(bit_start)}")
-                print(f"binary of word = {bin(nums[right])}")
-
                 bit_start |= nums[right]
                 
-                print(f"output = {bin(bit_start)}")
                 count += 1
                 right += 1
 
             longest = max(count, longest)
 
-            print("")
 
         return longest
