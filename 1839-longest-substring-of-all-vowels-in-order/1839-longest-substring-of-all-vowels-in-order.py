aaed1682a23e2ee6e241fc0ada09852b0899431f
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 12:52:35 +0000

    Time: 503 ms (23.27%), Space: 22.8 MB (28.16%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -6,19 +6,16 @@ class Solution:
         left = 0
         total = 0
 
-
         while left < len(word):
-
             target = 0
             right = left
-            while right < len(word) and (word[right] == vowels[target]):
+            while right < len(word) and word[right] == vowels[target]:
                 
                 if target < len(vowels) - 1 and right < len(word) - 1 and word[right + 1] != word[right]:
                     target += 1
                 
                 if word[right] == vowels[-1]:
                     total = max(total, right - left + 1)
-    
 
                 right += 1
 
