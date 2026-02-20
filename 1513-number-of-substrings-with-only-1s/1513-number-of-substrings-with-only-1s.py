337c1ec0e273aabe614ac355a6e9f63ea91bbd44
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 09:24:04 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.py
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.py
@@ -2,14 +2,17 @@ class Solution:
     def numSub(self, s: str) -> int:
         
         total = 0
+        left = 0
         right = 0
-        for i in range(len(s)):
-            if s[i] == "1":
-                while right < len(s) and s[right] == "1":
 
-                    total += right - i + 1
-                    right += 1
-
+        while right < len(s):
+            while right < len(s) and s[right] == "1":
+                print(s[left:right+1])
+                total += right - left + 1
+                right += 1
             else:
-                right = i + 1
+                right += 1
+
+            left = right
+
         return total % ((10**9) + 7)
