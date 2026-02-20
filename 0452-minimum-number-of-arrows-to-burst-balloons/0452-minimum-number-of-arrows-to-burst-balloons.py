c4cd7cf5a657c1730d153df887dbf3c139e06bd1
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 18:30:41 +0000

    Time: 95 ms (45.61%), Space: 53.6 MB (25.19%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -6,9 +6,6 @@ class Solution:
         points.sort(key=lambda x:x[0])
         last = points[0][1]
 
-        print(points)
-
-
         for i in range(1, len(points)):
             
             l, r = points[i][0], points[i][1]
