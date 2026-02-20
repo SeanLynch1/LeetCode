e8a810c1ae806a6616f54416607359c0212f6de3
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 19:22:21 +0000

    Time: 0 ms (100%), Space: 19.5 MB (5.12%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
@@ -7,6 +7,4 @@ class Solution:
         time_3 = int(event2[0][:2] + event2[0][3:])
         time_4 = int(event2[1][:2] + event2[1][3:])
 
-        print(time_1, time_2, time_3, time_4)
-
         return (time_2 >= time_3 and time_3 >= time_1) or (time_2 >= time_3 and time_4 >= time_1)
