8b1a00747da0fe2e1d60cde0ac3caeaef6a5a801
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 11:57:12 +0000

    Time: 1972 ms (33.34%), Space: 20.7 MB (42.08%) - LeetHub

---
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -0,0 +1,42 @@
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        
+        vowel_set = set("aieou")
+        n = len(vowel_set) + k
+
+        def countSubstrings(nk: int) -> int:
+            
+            total = 0
+            consonants = 0
+            vowels = defaultdict(int)
+
+            left = 0
+            right = 0
+
+            while right < len(word):
+                
+                letter = word[right]
+                if letter in vowel_set:
+                    vowels[letter] += 1
+                else:
+                    consonants += 1
+
+                while len(vowels) + consonants > nk or consonants > k:
+                    nxt_letter = word[left]
+
+                    if nxt_letter in vowel_set:
+                        vowels[nxt_letter] -= 1
+
+                        if vowels[nxt_letter] == 0:
+                            del vowels[nxt_letter]
+                    else:
+                        consonants -= 1
+
+                    left += 1
+
+                total += right - left + 1
+                right += 1
+
+            return total
+
+        return countSubstrings(n) - countSubstrings(n - 1)
