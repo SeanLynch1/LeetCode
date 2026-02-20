1ad6bc9af4f5021990111b01c7a55ac163102701
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-20 09:30:46 +0000

    Time: 19 ms (65.71%), Space: 19.8 MB (29.44%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.py
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.py
@@ -1,15 +1,14 @@
 class Solution:
     def numSub(self, s: str) -> int:
-        
+        MOD = 10**9 + 7
         total = 0
-        left = 0
-        right = 0
+        run = 0  # current streak length of '1's
 
-        while left < len(s):
-            while right < len(s) and s[right] == "1":
-                total += right - left + 1
-                right += 1
-            right += 1
-            left = right
+        for c in s:
+            if c == '1':
+                run += 1          # extend the current streak
+                total += run      # add all substrings ending here
+            else:
+                run = 0           # streak broken
 
-        return total % ((10**9) + 7)
+        return total % MOD
