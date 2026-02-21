32f705a74b1fe4bf551b13c68d0dc2408f32369c
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-21 12:03:00 +0000

    Time: 187 ms (39.92%), Space: 19.6 MB (63.95%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        
+
+        def findSubstringOfChar(char: str) -> int:
+            
+            longest = 1
+            left = 0
+            bank = k
+
+            for right in range(len(answerKey)):
+
+                if answerKey[right] != char:
+                    bank -= 1
+
+                    while bank < 0:
+                        if answerKey[left] != char:
+                            bank += 1
+
+                        left += 1
+                    
+                longest = max(longest, (right + 1) - (left))
+                right += 1
+
+            return longest
+
+        return max(findSubstringOfChar('T'), findSubstringOfChar('F'))
