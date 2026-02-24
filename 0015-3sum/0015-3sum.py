344560adae41f4f95f8089895cf83a85b93741b3
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-24 23:20:22 +0000

    Time: 623 ms (56.13%), Space: 22.2 MB (65.13%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,9 +4,10 @@ class Solution:
         output = []
         nums.sort()
         n = len(nums)
-
+        
         for start in range(n):
             
+            # skip duplicate first elements
             if start > 0 and nums[start] == nums[start - 1]:
                 continue
 
@@ -14,20 +15,26 @@ class Solution:
             left = start + 1
             right = n - 1
             
-            while right > left:
+            while left < right:
+                
                 s = nums[left] + nums[right] + target
-                if s == 0:
-                    output.append([target,  nums[left], nums[right]])
-                    left += 1
-                    right -=1
 
+                if s == 0:
+                    output.append([target, nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    # skip duplicate lefts
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
 
-                elif s >= 0:
-                    right -= 1
-                else:
+                    # skip duplicate rights  <-- FIXED
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif s < 0:
                     left += 1
-            
+                else:
+                    right -= 1
 
         return output
