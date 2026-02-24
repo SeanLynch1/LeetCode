983e23a1a00d357729d5fe4fb53b73177b87b294
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-24 22:24:48 +0000

    Time: 3 ms (52.65%), Space: 20.5 MB (36.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,11 +3,11 @@ class Solution:
         
         mapping = defaultdict(int)
 
-        for i in range(len(nums)):
-            mapping[nums[i]] = i
-
         for j in range(len(nums)):
             needed = target - nums[j]
 
             if needed in mapping and j != mapping[needed]:
                 return [j, mapping[needed]]
+
+            mapping[nums[j]] = j
+            
