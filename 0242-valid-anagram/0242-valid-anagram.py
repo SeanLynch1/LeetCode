5c35ffeaf0ffc3763daf8c6736acb07a62fa58c5
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-24 11:06:53 +0000

    Time: 11 ms (75.8%), Space: 19.5 MB (36.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        return Counter(s) == Counter(t)
+        if len(t) < len(s):
+            return False
+            
+        arr = [0] * 26
+
+        for ch in s:
+            alpha_idx = ord(ch) - ord('a')
+            arr[alpha_idx] += 1
+
+        for ch in t:
+            alpha_idx = ord(ch) - ord('a')
+
+            if arr[alpha_idx] == 0:
+                return False
+
+            arr[alpha_idx] -= 1
+        
+        return True
