ece138da46cc93e9daef96d85502c9826c6ce8cb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-24 20:40:51 +0000

    Time: 0 ms (100%), Space: 21.3 MB (71.35%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,7 +1,6 @@
 class Solution:
     def insert(self, intervals: List[List[int]], new: List[int]) -> List[List[int]]:
         l, r = new
-        placed = False
         res = []
 
         for start, end in intervals:
@@ -11,17 +10,14 @@ class Solution:
             
             # Case 2: interval is completely after new
             elif start > r:
-                if not placed:
-                    res.append([l, r])
-                    placed = True
-                res.append([start, end])
-
-            # Case 3: overlap
+                res.append([l, r])
+                l = max(l, start)
+                r = max(r, end)
             else:
+                # Case 3: overlap
                 l = min(l, start)
                 r = max(r, end)
 
-        if not placed:
-            res.append([l, r])
+        res.append([l, r])
 
         return res
