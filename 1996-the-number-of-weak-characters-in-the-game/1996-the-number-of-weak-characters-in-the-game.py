a1bfeff5707eda281f1592cba25343fbad83009e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-26 16:49:25 +0000

    Time: 435 ms (11.41%), Space: 60.4 MB (17.96%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -2,25 +2,19 @@ class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         
 
+        properties.sort(key=lambda x: (-x[0], x[1]))
         total = 0
-        # sort on attack key
-        properties.sort(key= lambda x: x[0])
 
         print(properties)
-        # we know last attack key is the highest value
+        max_def = properties[0][1]
+        
+        for i in range(1, len(properties)):
 
-        max_att = properties[-1][0]
-        max_def = 0
-
-        for i in range(len(properties)-1,-1,-1):
-            
-            att = properties[i][0]
             defence = properties[i][1]
+            
+            if defence < max_def:
+                total += 1
 
             max_def = max(max_def, defence)
-
-            if att < max_att and defence < max_def:
-                print(f"incrementing total, att = {att}, def = {defence}")
-                total += 1
 
         return total
