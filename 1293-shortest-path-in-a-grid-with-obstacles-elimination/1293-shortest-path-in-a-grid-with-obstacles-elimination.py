56cacfcf8424db2a185ec0bca35e1b1c348dfb9d
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-02-28 18:17:58 +0000

    Time: 383 ms (25.36%), Space: 28.5 MB (27.24%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,49 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        queue = deque([[0, 0, k, 0]])
+        visited = set()
+        total = 0
+
+        while queue:
+
+            for i in range(len(queue)):
+                x, y, bombs, total = queue.popleft()
+                
+                if x < 0 or x == rows:
+                    continue
+
+                if y < 0 or y == cols:
+                    continue
+                    
+                if (x, y, bombs) in visited:
+                    continue
+
+                visited.add((x, y, bombs))
+
+                if grid[x][y] == 1:
+                    bombs -= 1
+
+                    if bombs < 0:
+                        continue
+
+                if x == rows - 1 and y == cols -1:
+                    return total 
+                
+                # down
+                queue.append([x + 1, y, bombs, total + 1])
+                
+                # right
+                queue.append([x, y + 1, bombs, total + 1])
+
+                # left
+                queue.append([x, y - 1, bombs, total + 1])
+
+                # up
+                queue.append([x - 1, y, bombs, total + 1])
+
+        return -1
