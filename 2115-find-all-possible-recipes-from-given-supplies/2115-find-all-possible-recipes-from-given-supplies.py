499a4825e1e7f924a6c5499a31e0ece071453285
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-01 20:13:22 +0000

    Time: 175 ms (13.61%), Space: 41.7 MB (19.7%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,51 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        
+        output = []
+        self.mapping = defaultdict(list)
+        self.visited = defaultdict(int)
+
+        for supply in supplies:
+            self.mapping[supply] = []
+            self.visited[supply] = 0
+
+        
+        for i in range(len(recipes)):
+            for ingredient in ingredients[i]:
+                self.mapping[recipes[i]].append(ingredient)
+                self.visited[supply] = 0
+
+        
+        for key, value in self.mapping.items():
+            print(key, ": ", value)
+        
+        def traverse_recipes(item: str) -> bool:
+            
+            if item not in self.mapping:
+                return False
+
+            if self.visited[item] == 1:
+                return False
+
+            if not self.mapping[item] or self.visited[item] == 2:
+                self.visited[item] = 2
+                return True
+
+            self.visited[item] = 1
+
+            for ingredient in self.mapping[item]:
+
+                if traverse_recipes(ingredient):
+                    self.visited[ingredient] = 2
+                else:
+                    return False
+
+            self.visited[item] = 2
+
+            return True
+            
+        for recipe in recipes:
+            if traverse_recipes(recipe):
+                output.append(recipe)
+
+        return output
