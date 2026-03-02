7239a6cca65bd20d57a32f806b4d245494196df5
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-02 22:23:17 +0000

    Time: 3259 ms (11.49%), Space: 28.7 MB (5.22%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,53 @@
+class Node:
+
+    def __init__(self, start, end, idx):
+        self.start = start
+        self.end = end
+        self.idx = idx
+        self.next = None
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.curr = Node(1, w[0], 0)
+        start = self.curr
+
+        self.sum = w[0]
+
+        for i in range(1, len(w)):
+            interval = w[i]
+            s = self.sum + 1
+            e = self.sum + interval
+            self.sum += interval
+            
+            self.curr.next = Node(s, e, i)
+            self.curr = self.curr.next
+
+        self.curr = start
+
+        s = self.curr
+        while s:
+            print(f"start = {s.start} , end = {s.end} ,idx = {s.idx}")
+            s = s.next
+
+        print("\n")
+
+    def pickIndex(self) -> int:
+        print("")
+        curr = self.curr
+        random_no = random.randrange(1,self.sum + 1)
+        print(f"random_no = {random_no}")
+        while curr:
+            
+            if curr.start <= random_no and curr.end >= random_no:
+                print(f"{curr.start} <= {random_no} and {curr.end} >= {random_no}")
+                return curr.idx
+
+            curr = curr.next
+
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
