9ead83a6b0018cd9f2c1df9269490823378c9b20
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-02 22:41:45 +0000

    Time: 65 ms (59.51%), Space: 24.6 MB (57.28%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -9,16 +9,11 @@ class Solution:
             self.prefixes.append(self.sum + val)
             self.sum += val
 
-        print(self.prefixes)
-        print(self.sum)
-
     def pickIndex(self) -> int:
 
         random_no = random.randrange(0, self.sum)
 
-        print(f"random_no = {random_no}")
         idx = bisect_right(self.prefixes, random_no)
-        print(f"idx = {idx}")
         return idx - 1
         
 
