92dfa4bbc8a7ec4e3b029703527551177b307cbb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-24 12:46:08 +0000

    Time: 127 ms (26.36%), Space: 69 MB (5.65%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,39 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        # 1, 2
+        # 1, 3
+        # 1, 4
+        # 2, 3
+        # 3, 4
+        # 4, 1
+
+        # 1, 2, 3, 4
+
+        # 1, 2, 3
+        # 1, 2, 4
+        # 1, 3, 4
+        # 2, 3, 4
+        output = []
+        
+        def back_track(start:int, curr: list) -> None:
+            
+
+            if len(curr) < k:
+                curr.append(start)
+
+            if len(curr) == k:
+                output.append(curr)
+                return
+
+            for val in range(start + 1, n + 1):
+                pres = curr.copy()
+                back_track(val, curr)
+                curr = pres
+
+            return 
+
+        for i in range(1, n - k + 2):
+            back_track(i, [])
+
+        return output
