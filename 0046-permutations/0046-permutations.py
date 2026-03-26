88282a574fb0a423dbb6a11eb3f42d18f19527eb
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-26 10:37:29 +0000

    Time: 4 ms (10.36%), Space: 19.5 MB (77.97%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,29 @@
+from typing import List
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        output = []
 
-        def backtrack(path, remaining):
-            
-            if not remaining:
-                output.append(path)
+        output = []
+        ref_list = [False] * len(nums)
+
+        def back_track(path: List) -> None:
+
+            if len(path) == len(nums):
+                output.append(path.copy())
                 return
 
-            for i in range(len(remaining)):
-                backtrack(
-                    path + [remaining[i]], 
-                    remaining[:i] + remaining[i+1:]
-                )
+            for i in range(len(nums)):
 
-        backtrack([], nums)
+                if not ref_list[i]:
+                    path.append(nums[i])
+
+                    ref_list[i] = True
+
+                    back_track(path)
+
+                    path.pop()
+                    ref_list[i] = False
+
+        back_track([])    
+
         return output
