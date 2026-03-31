3b7690721bcdd8e23562eab4f09a820381dab67a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-31 17:10:53 +0100

    Time: 83 ms (5.07%), Space: 19.7 MB (29.72%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,28 +2,29 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         
         candidates.sort()
+        print(candidates)
 
-        visited = set()
         output = []
         def back_track(start, curr, path: List):
 
             if curr == target:
-                if tuple(path) not in visited:
-                    output.append(path.copy())
-                    visited.add(tuple(path))
+                output.append(path.copy())
                 return
 
             if curr > target:
                 return
-                
 
             for i in range(start, len(candidates)):
-
+                
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
                 val = candidates[i]
+                print(f"val = {val}, path = {path}")
 
                 path.append(val)
                 back_track(i + 1, curr + val, path)
                 path.pop()
+                
 
         back_track(0, 0, [])
         return output
