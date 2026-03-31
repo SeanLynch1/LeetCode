8d662b7b241dd4b5ef1b3947f36cee82f0852d33
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-31 10:39:46 +0100

    Time: 4205 ms (39.91%), Space: 19.6 MB (43.84%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -6,7 +6,7 @@ class Solution:
         rows = len(board)
         cols = len(board[0])
 
-        def search(x, y, idx, visited):
+        def search(x, y, idx):
             
             if idx == len(word):
                 self.output = True
@@ -18,29 +18,29 @@ class Solution:
             if board[x][y] != word[idx]:
                 return
             
-            if (x, y) in visited:
+            if (x, y) in self.visited:
                 return
 
-            visited.add((x,y))
+            self.visited.add((x,y))
 
             # right
-            search(x, y + 1, idx + 1,visited)
+            search(x, y + 1, idx + 1)
 
             # down
-            search(x + 1, y, idx + 1,visited)
+            search(x + 1, y, idx + 1)
 
             # left
-            search(x, y - 1, idx + 1, visited)
+            search(x, y - 1, idx + 1)
 
             # up
-            search(x - 1, y, idx + 1, visited)
+            search(x - 1, y, idx + 1)
 
-            visited.remove((x,y))
+            self.visited.remove((x,y))
 
-
+        self.visited = set()
         for r in range(rows):
             for c in range(cols):
-                search(r, c, 0, set())
+                search(r, c, 0)
                 
                 if self.output:
                     return True
