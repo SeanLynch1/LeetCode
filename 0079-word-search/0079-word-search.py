e0fe8ba460907d6532342c1a730ed6b38f4accec
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-03-31 10:41:50 +0100

    Time: 4265 ms (36.37%), Space: 19.7 MB (18.77%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,48 +1,47 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        self.output = False
-
         rows = len(board)
         cols = len(board[0])
 
-        def search(x, y, idx):
+        def search(x, y, idx) -> bool:
             
             if idx == len(word):
-                self.output = True
-                return
+                return True
 
             if x < 0 or y < 0 or x >= rows or y >= cols:
-                return
+                return False
 
             if board[x][y] != word[idx]:
-                return
+                return False
             
             if (x, y) in self.visited:
-                return
+                return False
 
             self.visited.add((x,y))
 
             # right
-            search(x, y + 1, idx + 1)
+            if search(x, y + 1, idx + 1):
+                return True
 
             # down
-            search(x + 1, y, idx + 1)
+            if search(x + 1, y, idx + 1):
+                return True
 
             # left
-            search(x, y - 1, idx + 1)
+            if search(x, y - 1, idx + 1):
+                return True
 
             # up
-            search(x - 1, y, idx + 1)
+            if search(x - 1, y, idx + 1):
+                return True
 
             self.visited.remove((x,y))
 
-        self.visited = set()
         for r in range(rows):
             for c in range(cols):
-                search(r, c, 0)
-                
-                if self.output:
+                self.visited = set()
+                if search(r, c, 0):
                     return True
         
         return False
