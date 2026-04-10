f17675a239e87cbf384477be06059066a97e3ed8
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-04-10 01:05:14 +0100

    Time: 6 ms (45.36%), Space: 19.8 MB (60.11%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
+        print(nums)
+        self.output = []
+        last = nums[0]
+
+        def back_track(path: List) -> None:
+
+            if len(path) == len(nums):
+                self.output.append(path.copy())
+                return
+
+            i = 0
+            while i < len(nums):
+                if nums[i] != '#':
+                    last = nums[i]
+
+                    path.append(nums[i])
+
+                    original = nums[i]
+                    nums[i] = '#'
+                    
+                    back_track(path)
+                    
+                    nums[i] = original
+                    path.pop()
+                
+                i += 1
+                
+                while i < len(nums) and nums[i] == nums[i - 1]:
+                    i += 1
+
+            return
+
+
+
+        back_track([])
+
+        return self.output
