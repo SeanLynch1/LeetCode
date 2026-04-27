86882ab841220d9efe8bdfd123c594f19d49f193
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-04-27 16:30:44 +0100

    Time: 2778 ms (25.59%), Space: 19.5 MB (29.19%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,42 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+
+        gold = 0
+
+        def dfs(row: int, col: int) -> int:
+            
+            if (row, col) in visited:
+                return 0
+
+            if (row == rows or row < 0) or (col == cols or col < 0):
+                return 0
+
+            if grid[row][col] == 0:
+                return 0
+
+            visited.add((row, col))
+
+            val = grid[row][col]
+            best = 0
+
+            dir = ((0,1),(1,0), (-1,0), (0,-1))
+            
+            for r, c in dir:
+                best = max(dfs(row + r, col + c), best)
+            
+            val += best
+            visited.remove((row, col))
+            
+            return val
+
+        for r in range(rows):
+            for c in range(cols):
+                # start points
+                gold = max(gold, dfs(r, c))
+                visited = set()
+
+        return gold
