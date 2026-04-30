f6f5d695523fa4055963db4c33bb9c15e34a9490
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-05-01 00:00:24 +0100

    Time: 1979 ms (46.96%), Space: 19.6 MB (57.62%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,51 +1,52 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
         row_sets = [set() for _ in range(9)]
         col_sets = [set() for _ in range(9)]
         box_sets = [set() for _ in range(9)]
 
-        # begin by initializing sets
+        empties = []
+
+        # initialize + collect empty cells
         for r in range(9):
             for c in range(9):
-                if board[r][c] != ".":
+                if board[r][c] == ".":
+                    empties.append((r, c))
+                else:
                     val = board[r][c]
                     row_sets[r].add(val)
                     col_sets[c].add(val)
                     box_sets[(r // 3) * 3 + (c // 3)].add(val)
-        
-        def dfs(r, c) -> bool:
-            if r == 9:
+
+        def dfs(i: int) -> bool:
+            # base case: all empties filled
+            if i == len(empties):
                 return True
 
-            if c == 9:
-                return dfs(r + 1, 0)
-            
-            
-            if board[r][c] == ".":
-                curr_box = (r // 3) * 3 + (c // 3)
+            r, c = empties[i]
+            box = (r // 3) * 3 + (c // 3)
 
-                for i in range(1, 10):
-                    s = str(i)  
+            for num in range(1, 10):
+                s = str(num)
 
-                    if s not in row_sets[r] and s not in col_sets[c] and s not in box_sets[curr_box]:
-                        board[r][c] = s
-                        row_sets[r].add(s)
-                        col_sets[c].add(s)
-                        box_sets[curr_box].add(s)
+                if (
+                    s not in row_sets[r]
+                    and s not in col_sets[c]
+                    and s not in box_sets[box]
+                ):
+                    board[r][c] = s
+                    row_sets[r].add(s)
+                    col_sets[c].add(s)
+                    box_sets[box].add(s)
 
-                        if dfs(r, c + 1):
-                            return True
-                        else:
-                            board[r][c] = "."
-                            row_sets[r].remove(s)
-                            col_sets[c].remove(s)
-                            box_sets[curr_box].remove(s)
-            else:
-                return dfs(r, c + 1)
+                    if dfs(i + 1):
+                        return True
+
+                    # backtrack
+                    board[r][c] = "."
+                    row_sets[r].remove(s)
+                    col_sets[c].remove(s)
+                    box_sets[box].remove(s)
 
             return False
 
-        dfs(0,0)
+        dfs(0)
