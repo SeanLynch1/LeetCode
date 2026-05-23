4e1217df03357d6e4955086f7d49cebaca0452c1
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-05-23 14:44:32 +0100

    Time: 48 ms (5.45%), Space: 19.1 MB (98.09%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.py
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.py
@@ -1,41 +1,31 @@
-from typing import List
-
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
         
-        rows = len(strs)
-        cols = len(strs[0])
-
-        # resolved[i] means:
-        # strs[i] < strs[i + 1] has already been confirmed
-        resolved = [False] * (rows - 1)
 
         deletions = 0
+        rows = len(strs) - 1
+        cols = len(strs[0])
 
-        # process columns left -> right
+        resolved_words = [False] * rows
+
+
         for col in range(cols):
 
-            # check whether keeping this column breaks ordering
             bad = False
 
-            for row in range(rows - 1):
-
-                # only compare unresolved pairs
-                if not resolved[row]:
-                    if strs[row][col] > strs[row + 1][col]:
+            for i in range(rows):
+                # compare forwards
+                if not resolved_words[i]:
+                    if strs[i][col] > strs[i + 1][col]:
                         bad = True
                         break
-
-            # must delete this column
+            
             if bad:
                 deletions += 1
                 continue
 
-            # otherwise keep the column
-            # and mark newly resolved pairs
-            for row in range(rows - 1):
-
-                if strs[row][col] < strs[row + 1][col]:
-                    resolved[row] = True
+            for i in range(rows):
+                if strs[i][col] < strs[i + 1][col]:
+                    resolved_words[i] = True
 
         return deletions
