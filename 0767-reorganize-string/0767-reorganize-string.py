75f5931b57c08a330fc8f7f4e72ffbee72993700
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-05-24 18:11:29 +0100

    Time: 5 ms (7.79%), Space: 19.4 MB (81.7%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -0,0 +1,36 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        
+        mapping = Counter(s)
+        heap = []
+        word = []
+
+        for key, val in mapping.items():
+
+            heappush(heap, [-val, key])
+
+        print(heap)
+
+        while heap:
+
+            node_1 = heappop(heap)
+
+            word.append(node_1[1])
+            node_1[0] += 1
+
+            if heap:
+                node_2 = heappop(heap)
+
+                word.append(node_2[1])
+                node_2[0] += 1
+
+                if node_2[0] < 0:
+                    heappush(heap, node_2)
+
+                if node_1[0] < 0:
+                    heappush(heap, node_1)
+
+            
+
+        print(f"heap = {heap}")
+        return "".join(word) if len(word) == len(s) else ""
