516f444a5c9d28cfabf8811cabb4c9970e704ef9
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-01 13:48:29 +0100

    Time: 352 ms (62.36%), Space: 20.2 MB (18.4%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,40 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        # [[10, 20], [50, 60]]
+        # [[10, 40], ]
+
+        self.bookings = []
+        self.overlaps = []
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        print(f"{startTime}, {endTime}")
+        
+        for slot in self.overlaps:
+            if startTime >= slot[0] and startTime < slot[1]:
+                return False
+            elif endTime > slot[0] and endTime <= slot[1]:
+                return False
+            elif startTime <= slot[0] and endTime >= slot[1]:
+                return False
+            
+        min_left = startTime
+        max_right = endTime
+        #[10, 20] [9,21]
+        for slot in self.bookings:
+            if startTime <= slot[0] and endTime >= slot[1]:
+                self.overlaps.append([slot[0], slot[1]])
+                
+            elif startTime >= slot[0] and startTime < slot[1] or endTime > slot[0] and endTime <= slot[1]:
+                min_left = max(startTime, slot[0])
+                max_right = min(endTime, slot[1])
+                self.overlaps.append([min_left, max_right])
+
+        self.bookings.append([startTime, endTime])
+        
+        return True
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(startTime,endTime)
