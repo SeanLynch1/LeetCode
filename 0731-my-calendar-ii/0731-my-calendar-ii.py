beb48fdae5148bf900ad156c44bc2bd088ca4a28
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-01 17:21:47 +0100

    Time: 2296 ms (5.01%), Space: 20.2 MB (41.42%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -4,12 +4,11 @@ class MyCalendarTwo:
         # [[10, 20], [50, 60]]
         # [[10, 40], ]
 
-        self.bookings = []
-        self.overlaps = []
+        self.bookings = defaultdict(int)
 
     def book(self, startTime: int, endTime: int) -> bool:
         
-        for slot in self.overlaps:
+        '''for slot in self.overlaps:
             if startTime >= slot[0] and startTime < slot[1]:
                 return False
             elif endTime > slot[0] and endTime <= slot[1]:
@@ -29,11 +28,34 @@ class MyCalendarTwo:
                 max_right = min(endTime, slot[1])
                 self.overlaps.append([min_left, max_right])
 
-        self.bookings.append([startTime, endTime])
-        
+        self.bookings.append([startTime, endTime])'''
+        self.bookings[startTime] += 1
+        self.bookings[endTime] -= 1
+        active = 0
+
+        for key in sorted(self.bookings):
+            val = self.bookings[key]
+            active += val
+
+            if active > 2:
+                self.bookings[startTime] -= 1
+                self.bookings[endTime] += 1
+                return False
         return True
 
+'''[[1,20],[5,15],[13,17],[18,20],[0,21]]
 
+    {
+    0: 1,
+    1: 1,
+    5: 1,
+    13: 1,
+    15: -1,
+    17: -1,
+    18: 1,    
+    20: -2,
+    21: -1}
+'''
 # Your MyCalendarTwo object will be instantiated and called as such:
 # obj = MyCalendarTwo()
 # param_1 = obj.book(startTime,endTime)
