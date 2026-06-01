85df51c3c9f12686df989d7b4145f53ae0bdd728
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-01 17:22:32 +0100

    Time: 1128 ms (18.59%), Space: 20.1 MB (75.85%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -40,6 +40,11 @@ class MyCalendarTwo:
             if active > 2:
                 self.bookings[startTime] -= 1
                 self.bookings[endTime] += 1
+
+                if self.bookings[startTime] == 0:
+                    del self.bookings[startTime]
+                if self.bookings[endTime] == 0:
+                    del self.bookings[endTime]
                 return False
         return True
 
