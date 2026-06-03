3e9ee1ba362c657078f0ad59fabf6a0baf5b8966
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-03 17:09:24 +0100

    Time: 3 ms (32.94%), Space: 19.4 MB (28.22%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        
+        years = []
+
+        for birth, death in logs:
+            years.append([birth, 1])
+            years.append([death, -1])
+
+        years.sort(key=lambda x: (x[0], x[1]))
+
+        ans = years[0][0]
+        max_curr = 1
+        curr = 0
+
+        print(years)
+        for slot in years:
+            curr += slot[1]
+            if curr > max_curr:
+                max_curr = curr
+                ans = slot[0]
+        return ans
