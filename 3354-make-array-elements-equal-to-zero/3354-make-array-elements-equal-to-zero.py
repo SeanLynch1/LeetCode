bff718a948cd9dc71328d69ff8d996fd89cde966
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-04 23:47:30 +0100

    Time: 55 ms (45.18%), Space: 19.3 MB (53.3%) - LeetHub

---
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        
+        #  [1,0,2,0,3]
+        #  [0,1,1,3,3,6]
+
+        prefixes = [0]
+
+        for num in nums:
+            prefixes.append(prefixes[-1] + num)
+
+        print(prefixes)
+
+        suffix = 0
+        ans = 0
+
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i] == 0:
+                diff = abs(prefixes[i] - suffix)
+                if diff == 1:
+                    ans += 1
+                elif diff == 0:
+                    ans += 2
+
+            suffix += nums[i]
+        return ans
+
