97fe72548e4c0e6c34cab10d20df09dde4627974
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-06 22:33:47 +0100

    Time: 8 ms (72.59%), Space: 19.8 MB (7.73%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,15 +1,20 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        #[9, 16, 11, 18]
+
+        #[2,2,3,4,6, 6, 7, 8,   ]
+        #[0,2,4,7,11,17,23,30,38]
+
+        #[1,2,2,5,5, 6, 7, 8,   ]
+        #[0,1,3,5,10,15,21,28,36]
+
         nums.sort()
-        total = 0
-        prefixes = []
+        prefixes = [0]
         ans = []
+        for i in range(len(nums)):
+            prefixes.append(prefixes[-1] + nums[i])
+        print(prefixes)
         
-        for i, n in enumerate(nums):
-            total += n
-            prefixes.append(total)
-
         for q in queries:
-            ans.append(bisect_right(prefixes, q))
-
+            ans.append(bisect_right(prefixes,q)-1)
         return ans
