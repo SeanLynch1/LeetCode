fced8c460a66151aeac9eb2ed74d30d06f519420
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-14 17:02:22 +0100

    Time: 301 ms (15.09%), Space: 66.6 MB (41.86%) - LeetHub

---
+++ 3919-minimum-cost-to-move-between-indices/3919-minimum-cost-to-move-between-indices.py
@@ -0,0 +1,63 @@
+class Solution:
+    def minCost(self, nums: list[int], queries: list[list[int]]) -> list[int]:
+        
+        #strt  [-5,-2,3,5]
+        #left  [0 , 3,5,2]
+        #right [3 , 5,2,0]
+
+        #left  [0, 1, 5,1]
+        #right [1, 5, 1,0]
+
+        #left  [7, 6,1, 0]
+        #right [0,1, 6, 7]
+
+        ans = []
+        n = len(nums)
+        left = [0] * n
+        right = [0] * n
+
+        for i in range(n):
+            if i - 1 >= 0:
+                left[i] = abs(nums[i] - nums[i-1])
+            if i + 1 < n:
+                right[i] = abs(nums[i] - nums[i+1])
+            
+        print(f"left  = {left}")
+        print(f"right = {right}")
+
+        prefixes = [0]
+
+        for i in range(0, n-1):
+            if i == 0:
+                prefixes.append(1)
+            elif left[i] > right[i]:
+                prefixes.append(prefixes[-1] + 1)
+            else:
+                prefixes.append(prefixes[-1] + right[i])
+
+        print(prefixes)
+        
+        suffixes = [0] * n
+
+        for i in range(n-1,0,-1):
+            if i == n-1:
+                suffixes[i-1] = 1
+            elif right[i] >= left[i]:
+                suffixes[i-1] = suffixes[i] + 1
+            else:
+                suffixes[i-1] = suffixes[i] + left[i]
+
+        print(suffixes)
+
+        for l, r in queries:
+            total = 0
+            # left to right
+            if r > l:
+                total = abs(prefixes[r] - prefixes[l])
+            
+            elif l > r:
+                total = abs(suffixes[l] - suffixes[r])
+
+            ans.append(total)
+
+        return ans
