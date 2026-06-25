f5552ec2dc2b34be2321751b8bc5ecd08471d2b2
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-26 00:09:27 +0100

    Time: 552 ms (24.1%), Space: 59.8 MB (6.02%) - LeetHub

---
+++ 3755-find-maximum-balanced-xor-subarray-length/3755-find-maximum-balanced-xor-subarray-length.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxBalancedSubarray(self, nums: List[int]) -> int:
+        max_len = 0
+
+        mapping = defaultdict(dict)
+        mapping[0][0] = 0
+
+        odds = 0
+        xor_val = 0
+
+        for i in range(len(nums)):
+            num = nums[i]
+            if num % 2 == 1:
+                odds += 1
+            else:
+                odds -= 1
+
+            xor_val = xor_val ^ num
+
+            if xor_val in mapping[odds]:
+
+                idx = mapping[odds][xor_val]
+
+                max_len = max(max_len, i + 1 - idx)
+            
+            if xor_val not in mapping[odds]:
+                mapping[odds][xor_val] = i + 1
+
+        return max_len
