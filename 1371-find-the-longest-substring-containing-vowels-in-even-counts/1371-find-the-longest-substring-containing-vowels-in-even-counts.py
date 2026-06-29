38617f0286edcc86e9e235ddd25d2faaad7c2b8f
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-29 22:06:59 +0100

    Time: 279 ms (18.63%), Space: 29.4 MB (5.39%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -3,8 +3,23 @@ class Solution:
 
         max_len = 0
         
+        #1132211
+
+        # 0001 1
+        # 0001 and 1
+        # 0000 makes 0
+        # 0011 3
+        # 0010 and 2
+        # 0001 makes 1
+        # 0010 and 2
+        # 0011 makes 3
+        # 0001 and 1
+        # 0010 makes 2
+        # 0001 and 1
+        # 0011 makes 3
+
         mapping = {0: 0}
-        vowels = {'a','i','e','o','u'}
+        vowels = {'a':1,'i':2,'e':3,'o':4,'u':5}
 
         n = len(s)
         xors = [0] * (n + 1)
@@ -15,7 +30,7 @@ class Solution:
             val = xors[i]
 
             if letter in vowels:
-                val ^= ord(letter)
+                val ^= 1 << vowels[letter]
                 xors[i + 1] = val
 
                 if val not in mapping:
@@ -25,6 +40,9 @@ class Solution:
 
             if val in mapping:
                 max_len = max(max_len, i - mapping[val] + 1)
+
+        print(xors)
+        print(mapping)
 
         return max_len
 
