429441ad1628f7fc69310e037d215a22f8b7d80a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-06-29 21:12:55 +0100

    Time: 99 ms (16.44%), Space: 24.9 MB (97.44%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,18 +1,17 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         
-        mapping = defaultdict(list)
-        mapping[0] = [0]
-        
+        mapping = {0:0}
         max_len = 0
         balance = [0]
 
         for idx in range(len(nums)):
             val = balance[-1] + (1 if nums[idx] == 1 else -1)
             balance.append(val)
-            mapping[val].append(idx + 1)
-        
-        for key, val in mapping.items():
-            max_len = max(max_len, val[-1] - val[0])
+
+            if val not in mapping:
+                mapping[val] = idx + 1
+            else:
+                max_len = max(max_len, idx - mapping[val] + 1)
             
         return max_len
