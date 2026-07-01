fd4749c15455c689f78a6afa2f4e8f0453c02a8e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-07-01 14:43:45 +0100

    Time: 304 ms (5.09%), Space: 20.5 MB (56.07%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,23 +1,41 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int):
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        # k = 2
+        #[1,0,0,1,1]
 
-        def atMost(k):
-            if k < 0:
+        # sum <= 2
+        # 1
+        # 10, 0
+        # 100, 00, 0
+        # 1001,001,01,1
+        # 0011,011,11,1
+
+        # sum <= 1
+        # 1
+        # 10, 0
+        # 100, 00, 0
+        # 001, 01, 1
+        # 011, 11, 1
+
+        def atMost(k:int):
+            if k == -1:
                 return 0
 
-            left = 0
-            curr = 0
             total = 0
+            curr = 0
+            left = 0
 
             for right in range(len(nums)):
                 curr += nums[right]
-
+                print(f"curr = {curr}, k = {k}")
                 while curr > k:
                     curr -= nums[left]
                     left += 1
+                    print(f"left = {left}")
 
                 total += right - left + 1
-
+                print("")
             return total
 
-        return atMost(goal) - atMost(goal - 1)
+        return atMost(goal) - atMost (goal - 1)
