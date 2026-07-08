855aafa6df078938f6eef9c5ea39571cec045c7b
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-07-08 11:56:05 +0100

    Time: 59 ms (5.36%), Space: 24.1 MB (21.37%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -0,0 +1,34 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        
+        # [4,5,0,-2,-3,1]
+        # [0,4,9, 9, 7,4,5]
+        # 5 - 0 = [4,5,0,-2,-3,1]
+        # 4 - 9 = [-2,-3]
+        # 4 - 9 = [0,-2,-3]
+        # 4 - 4 = [5,0,-2,-3]
+        # 9 - 9 = [0]
+        # 9 - 4 = [5,0]
+        # 9 - 4 = [5]
+
+        res = 0
+        prefix = [0]
+        mapping = defaultdict(int)
+        mapping[0] += 1
+
+        for i, num in enumerate(nums):
+            val = prefix[-1] + num
+            prefix.append(val)  
+
+            remainder = val % k
+
+            if remainder in mapping:
+                res += mapping[remainder]
+            mapping[remainder] += 1
+
+        print(f"prefixes = {prefix}")
+        print(f"mapping = {mapping}")
+        print("")
+
+        
+        return res
