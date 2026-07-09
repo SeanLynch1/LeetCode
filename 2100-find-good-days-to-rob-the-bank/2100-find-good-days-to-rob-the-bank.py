21f32093cc4e59b97b3aa29f416f3c765735077e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-07-09 15:18:19 +0100

    Time: 148 ms (14%), Space: 38.7 MB (29.33%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -0,0 +1,45 @@
+class Solution:
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        
+        # [ 5, 7, 7, 6, 3, 2, 3, 4, 1, 4, 1] time = 2
+        # [-1, 0, 1, 2, 3, 4, 3, 2, 3, 2, 3]
+        res = []
+        n = len(security) 
+        prefix = [0] * n
+        curr = 0
+        d = 0
+        lst = security[0]
+
+        for i in range(1, n):
+            num = security[i]
+
+            if num <= lst:
+                curr += 1
+            else:
+                curr = 0
+
+            prefix[i] = curr
+            lst = num
+
+        print(f"prefix = {prefix}")
+
+        suffix = [0] * n
+        lst = security[-1]
+
+        for i in range(n-1,-1,-1):
+            num = security[i]
+
+            if num <= lst:
+                curr += 1
+            else:
+                curr = 0
+
+            suffix[i] = curr
+            lst = num
+
+            if i - time >= 0 and i + time < n:
+                if prefix[i] - prefix[i - time] == time:
+                    if suffix[i] - suffix[i + time] == time:
+                        res.append(i)
+        print(f"suffix = {suffix}")
+        return res
