5536e75cb9f7591436fdcc31ba246401984a7e51
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-07-09 10:51:22 +0100

    Time: 319 ms (56.07%), Space: 26.6 MB (43.93%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,52 +1,65 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
         
-        # [1,1,3,2,4,3,2]
-        # [1,1,3,2,4,3,2]
-        # [1,1,3,2,4,3,2]
-
-        # [0,0,0,0 ,0 , 0, 0, 0]
-        # [0,1,2,5 ,6 ,11,14,15]
-        # [0,2,4,10,14,22,28,32]
-        # [0,3,6,15,21,33,42,48]
-
-        # [10,10,10,10,10,10]
-        # [10,10,10,10, 1, 1]
-        # [10,10,10,10, 1, 1]
-
-        # [0,  0,  0,  0,   0,   0,   0]
-        # [0, 10, 20, 30,  40,  50,  60]
-        # [0, 20, 40, 60,  80,  91, 102]
-        # [0, 30, 60, 90, 120, 132, 144]
-
-        res = 0
-
+        # if a 3 x 3 square passes, then any smaller square will pass too... binary search
+         
         rows = len(mat) + 1
         cols = len(mat[0]) + 1
 
-        prefixes = [[0] * (cols) for _ in range(rows)]
+        prefixes = [[0] * cols for _ in range(rows)]
+
+        # [[1,1,3,2,4,3,2],
+        #  [1,1,3,2,4,3,2],
+        #  [1,1,3,2,4,3,2]]
+
+        #[[10,10,10,10,10,10], 
+        # [10,10,10,10, 1, 1], 
+        # [10,10,10,10, 1, 1]]
 
         for row in range(1, rows):
-            for col in range(1, cols): 
-                prefixes[row][col] += mat[row-1][col-1]
-                prefixes[row][col] += prefixes[row][col-1]
-                prefixes[row][col] += prefixes[row - 1][col]
-                prefixes[row][col] -= prefixes[row - 1][col-1]
+            for col in range(1, cols):
+                prefixes[row][col] = (
+                    prefixes[row][col-1] +
+                    prefixes[row - 1][col] +
+                    mat[row-1][col-1] -
+                    prefixes[row - 1][col - 1]
+                )
+
+            print(prefixes[row-1])           
+        print(prefixes[row])
+
+        def can_make(side):
+
+            for row in range(side, rows):
+                for col in range(side, cols):
+
+                    total = (
+                        prefixes[row][col] -
+                        prefixes[row - side][col] -
+                        prefixes[row][col - side]
+                        + prefixes[row - side][col - side]
+                    )
+
+                    if total <= threshold:
+                        return True
+
+            return False
 
         min_side = min(rows, cols)
 
-        for i in range(min_side):
-            side = i + 1
-            for row in range(side, rows):
-                for col in range(side, cols):
-                    
-                    total = (prefixes[row][col]
-                            - prefixes[row][col-side]
-                            - prefixes[row - side][col]
-                            + prefixes[row - side][col - side]
-                            )
+        left = 0
+        right = min_side
 
-                    if total <= threshold:
-                        res = max(res, side)
+        while left <= right:
 
-        return res
+            mid = (left + right) // 2
+
+            if can_make(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
+
+
+
