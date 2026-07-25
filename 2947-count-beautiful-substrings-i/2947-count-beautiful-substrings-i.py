9c34444a4c9afbab2684214a8f121284c5ea631c
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-07-25 18:02:26 +0100

    Time: 34 ms (92.31%), Space: 19.6 MB (1.28%) - LeetHub

---
+++ 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.py
@@ -0,0 +1,49 @@
+class Solution:
+    def beautifulSubstrings(self, s: str, k: int) -> int:
+        
+        answer = 0
+
+        # define min beautiful sub string length
+        x = 1
+        while (x * x) % k != 0:
+            x += 1
+
+        min_len = x * 2
+        
+        print(min_len)
+        # k = 1, min len = 2
+        # k = 2, min len = 4
+        # k = 3, min len = 6
+        # k = 4, min len = 4
+        # k = 5, min len = 10
+        # k = 6, min len = 12
+        # k = 7, min len = 14
+        # k = 8, min len = 8
+        # k = 9, min len = 18
+        # k = 10,min len = 20
+
+        visited = defaultdict(int)
+        visited[(0,0)] = 1
+
+        vowels = {'a','e','i','o','u'}
+        balance = 0
+
+        for i in range(len(s)):
+            ch = s[i]
+
+            if ch in vowels:
+                balance += 1
+            else:
+                balance -= 1
+
+            remainder = (i + 1) % min_len
+
+            answer += visited[(balance, remainder)]
+
+            visited[(balance, remainder)] += 1
+
+
+        for key, val in visited.items():
+            print(f"{key}, {val}")
+
+        return answer  
