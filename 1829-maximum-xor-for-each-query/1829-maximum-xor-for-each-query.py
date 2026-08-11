72211910724010213c62535fed4af0f1cebba421
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-08-11 16:00:29 +0100

    Time: 59 ms (43.35%), Space: 36.7 MB (7.22%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -1,20 +1,5 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        
-        # 0010
-        # 0011
-        #
-        # 0001 -> add 1
-        # 0100
-        #
-        # 0101 -> add 5
-        # 0111
-        #
-        # 0010 -> add 2
-        # 0101 -> 5 (needed)
-        #
-        # 0111
-
         target = (2 ** maximumBit) - 1
         prefixes = [0]
         output = []
