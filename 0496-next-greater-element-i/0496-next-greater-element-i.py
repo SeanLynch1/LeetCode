352665975826a0e939fd88752a74b4cf83ed069a
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-08-13 10:22:55 +0100

    Time: 0 ms (100%), Space: 19.2 MB (92.48%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        mapping = defaultdict(int)
+        ans = [-1] * len(nums1)
+
+        for idx, num in enumerate(nums1):
+            mapping[num] = idx
+
+        for i in range(len(nums2)):
+            first_num = nums2[i]
+
+            if first_num not in mapping:
+                continue
+
+            for j in range(i, len(nums2)):
+                second_num = nums2[j]
+                if second_num > first_num:
+                    ans[mapping[first_num]] = second_num
+                    break
+
+        return ans
