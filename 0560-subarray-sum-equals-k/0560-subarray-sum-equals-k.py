2b4c47ec598dadd5633823b8952c61ab6e245e21
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-09-03 14:32:01 +0100

    Time: 139 ms (5.46%), Space: 24.7 MB (5.73%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        # [1,2,3,2,3,4 ,1 ,2 ]
+
+        # [0,1,3,6,8,11,15,16,18]
+
+        mapping = defaultdict(int)
+        mapping[0] = 1
+        prefixes = [0]
+        output = 0
+
+        for i in range(len(nums)):
+            val = prefixes[-1] + nums[i]
+            prefixes.append(val)
+
+            needed = val - k
+            print(f"needed = {needed}")
+            output += mapping[val - k]
+            mapping[val] += 1
+
+
+        print(mapping)
+
+        return output
