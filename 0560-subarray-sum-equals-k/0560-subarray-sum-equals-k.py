b144350fe2ed7d84751052a9bb61d2e077ed36b1
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-09-03 14:34:55 +0100

    Time: 42 ms (19.47%), Space: 24 MB (5.73%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -7,13 +7,14 @@ class Solution:
 
         mapping = defaultdict(int)
         mapping[0] = 1
-        prefixes = [0]
+        prefixes = [0] * (len(nums) + 1)
         output = 0
-
+        last = 0
         for i in range(len(nums)):
-            val = prefixes[-1] + nums[i]
-            prefixes.append(val)
-
+            val = last + nums[i]
+            prefixes[i + 1] = val
+            last = val
+            
             output += mapping[val - k]
             mapping[val] += 1
 
