24165215a7a0ced94f5c9036ecbe4e721a1aa134
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-09-04 22:00:35 +0100

    Time: 7 ms (74.43%), Space: 19.3 MB (24.43%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -7,14 +7,14 @@ class Solution:
         # [0,1,7,0,3]
 
         output = 0
-        prefixes = [0]
-
-        for num in arr:
-            prefixes.append(prefixes[-1] ^ num)
 
         for i in range(len(arr)):
-            for j in range(i + 1, len(arr)):
-                if prefixes[i] == prefixes[j + 1]:
+            xor = 0
+
+            for j in range(i, len(arr)):
+                xor ^= arr[j]
+
+                if xor == 0:
                     output += j - i 
 
         return output
