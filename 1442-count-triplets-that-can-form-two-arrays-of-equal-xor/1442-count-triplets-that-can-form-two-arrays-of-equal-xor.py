3e086e726ee6b36c74ffe323188d15e4072ca45d
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2026-09-05 18:14:18 +0100

    Time: 0 ms (100%), Space: 19.3 MB (65.34%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,6 +1,27 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
+        
         # [2,3,1,6,7,3,2]
+        # [0,2,1,0,6,1,2,0]
+
+        counts = defaultdict(list)
+        counts[0] = [0]
+        prefixes = [0]
+
+        output = 0
+
+        for i, num in enumerate(arr):
+            val = prefixes[-1] ^ num
+            prefixes.append(val)
+
+            if val in counts:
+                for j in counts[val]:
+                    output += i - j 
+            counts[val].append(i + 1)
+
+        return output
+
+        '''# [2,3,1,6,7,3,2]
         # [0,2,1,0,6,1,2,0]
 
         # [2,4,1,6,7,3,2]
@@ -23,4 +44,4 @@ class Solution:
             counts[xor] += 1
             index_sum[xor] += i + 1
 
-        return output
+        return output'''
